ce020ba2b3c1c01bb4d3c961ef21fe99dd186d27
Minghao Chi (CGEL ZTE) <chi.minghao@zte.com.cn>
2022-02-21 02:01:03 +0000

    UPSTREAM: clk/rockchip: Use of_device_get_match_data()
    Use of_device_get_match_data() to simplify the code.
    
    Change-Id: I5393c71b42e9f5c4d8acac1d2d07aab3833b3c34
    Reported-by: Zeal Robot <zealci@zte.com.cn>
    Signed-off-by: Minghao Chi (CGEL ZTE) <chi.minghao@zte.com.cn>
    Link: https://lore.kernel.org/r/20220221020103.1925026-1-chi.minghao@zte.com.cn
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    (cherry picked from commit 9f565399ad0739dbdeac868e40e86e80f54dc77b)

--- drivers/clk/rockchip/clk-rk3568.c
+++ drivers/clk/rockchip/clk-rk3568.c
@@ -1761,14 +1761,12 @@ MODULE_DEVICE_TABLE(of, clk_rk3568_match_table);
 static int clk_rk3568_probe(struct platform_device *pdev)
 {
 	struct device_node *np = pdev->dev.of_node;
-	const struct of_device_id *match;
 	const struct clk_rk3568_inits *init_data;
 
-	match = of_match_device(clk_rk3568_match_table, &pdev->dev);
-	if (!match || !match->data)
+	init_data = (struct clk_rk3568_inits *)of_device_get_match_data(&pdev->dev);
+	if (!init_data)
 		return -EINVAL;
 
-	init_data = match->data;
 	if (init_data->inits)
 		init_data->inits(np);
 
