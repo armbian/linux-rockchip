f2052085854ab80d2c7a1982eda4479989d1d83b
Stephen Chen <stephen@radxa.com>
2023-01-28 19:09:25 +0800

    ethernet: stmmac: add phy rtl8211f register fixup
    RTL8211F LED indication
      * LED1(Green) LED : Link + Active
      * LED2(Yellow) LED: Link
    
    Signed-off-by: Stephen Chen <stephen@radxa.com>
    (cherry picked from commit 9ffaf37a55345eb3c2f70aca00eaa0d88876c53e)
    Signed-off-by: Joshua Riek <jjriek@verizon.net>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -57,6 +57,12 @@
 #define	STMMAC_ALIGN(x)		ALIGN(ALIGN(x, SMP_CACHE_BYTES), 16)
 #define	TSO_MAX_BUFF_SIZE	(SZ_16K - 1)
 
+#define	RTL8211F_PHY_ID		0x001cc916
+#define	RTL8211F_PHY_ID_MASK	0x001fffff
+#define	RTL8211F_PAGE_SELECT	0x1f
+#define	RTL8211F_LCR_ADDR	0x10
+#define	RTL8211F_EEELCR_ADDR	0x11
+
 /* Module parameters */
 #define TX_TIMEO	5000
 static int watchdog = TX_TIMEO;
@@ -4994,6 +5000,33 @@ int stmmac_reinit_ringparam(struct net_device *dev, u32 rx_size, u32 tx_size)
 	return ret;
 }
 
+static int phy_rtl8211f_led_fixup(struct phy_device *phydev)
+{
+	u32 val, val2;
+
+	/* Switch to Page 0x0d04 */
+	phy_write(phydev, RTL8211F_PAGE_SELECT, 0x0d04);
+
+	/* Set LED1(Green) Link 10/100/1000M + Active, and set LED2(Yellow) Link 10/100/1000M */
+	val = phy_read(phydev, RTL8211F_LCR_ADDR);
+	val |= (1<<5);
+	val |= (1<<8);
+	val |= (1<<10);
+	val |= (1<<11);
+	val &= (~(1<<14));
+	phy_write(phydev, RTL8211F_LCR_ADDR, val);
+
+	/* Disable LED2(Yellow) EEE LED function to keep it on when linked */
+	val2 = phy_read(phydev, RTL8211F_EEELCR_ADDR);
+	val2 &= (~(1<<3));
+	phy_write(phydev, RTL8211F_EEELCR_ADDR, val2);
+
+	/* Switch back to the PHY's IEEE Standard Registers. Here it is Page 0 */
+	phy_write(phydev, RTL8211F_PAGE_SELECT, 0);
+
+	return 0;
+}
+
 /**
  * stmmac_dvr_probe
  * @device: device pointer
@@ -5235,6 +5268,12 @@ int stmmac_dvr_probe(struct device *device,
 	 * If CONFIG_PM is not enabled, the clocks will stay powered.
 	 */
 	pm_runtime_put(device);
+
+	/* Register fixup for PHY RTL8211F */
+	ret = phy_register_fixup_for_uid(RTL8211F_PHY_ID, RTL8211F_PHY_ID_MASK, phy_rtl8211f_led_fixup);
+	if (ret) {
+		dev_warn(priv->device, "Failed to register fixup for PHY RTL8211F.\n");
+	}
 
 	return ret;
 
