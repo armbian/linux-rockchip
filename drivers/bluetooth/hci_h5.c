0188c7d5f6130029d5bf7e7173f4f9050018edeb
Chris Morgan <macromorgan@xxxxxxxxxxx>
2023-02-16 20:14:54 +0100

    9Tripod / Revise hardware tables
    RTL8821CS is a WiFi + Bluetooth combo chip from Realtek that provides
    WiFi A/B/G/N/AC over an SDIO interface and Bluetooth 4.2 over a UART
    interface.
    
    Note that the firmware this was tested with was firmware version
    0xaa9a4e68 as reported by the driver. Attempts at using other
    firmware were unsucessful.
    
    Adapted for rockchip-5.10-rkr3.6 kernel.
    
    Signed-off-by: Chris Morgan <macromorgan@xxxxxxxxxxx>
    Signed-off-by: Steve Hay <me@stevenhay.com>

--- drivers/bluetooth/hci_h5.c
+++ drivers/bluetooth/hci_h5.c
@@ -1022,6 +1022,8 @@ static const struct dev_pm_ops h5_serdev_pm_ops = {
 
 static const struct of_device_id rtl_bluetooth_of_match[] = {
 #ifdef CONFIG_BT_HCIUART_RTL
+	{ .compatible = "realtek,rtl8821cs-bt",
+	  .data = (const void *)&rtl_vnd },
 	{ .compatible = "realtek,rtl8822cs-bt",
 	  .data = (const void *)&rtl_vnd },
 	{ .compatible = "realtek,rtl8723bs-bt",
