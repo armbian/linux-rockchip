180417158bf994fcda4ca7a6d63ee3e2caf9af6e
Joshua-Riek <jjriek@verizon.net>
2023-11-01 21:33:53 -0400

    ethernet: realtek: r8125: stop rss spamming kernel logs

--- drivers/net/ethernet/realtek/r8125/r8125_rss.c
+++ drivers/net/ethernet/realtek/r8125/r8125_rss.c
@@ -88,7 +88,7 @@ int rtl8125_get_rxnfc(struct net_device *dev, struct ethtool_rxnfc *cmd,
         struct rtl8125_private *tp = netdev_priv(dev);
         int ret = -EOPNOTSUPP;
 
-        netif_info(tp, drv, tp->dev, "rss get rxnfc\n");
+        netif_dbg(tp, drv, tp->dev, "rss get rxnfc\n");
 
         if (!(dev->features & NETIF_F_RXHASH))
                 return ret;
