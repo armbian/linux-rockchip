c83f6f3c427df8ba41a67c1bb92984ddd467262c
Joshua Riek <jjriek@verizon.net>
2023-12-21 11:40:35 -0500

    drivers: drm: dw-hdmi-qp: Add ddc-i2c-rxfilter DT prop

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi-qp.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi-qp.c
@@ -1085,6 +1085,7 @@ static const struct hdmi_quirk *get_hdmi_quirk(const u8 *vendor_id)
 static void dw_hdmi_i2c_init(struct dw_hdmi_qp *hdmi)
 {
 	void *data = hdmi->plat_data->phy_data;
+	u32 ddc_i2c_rxfilter;
 	u64 scl_high_cnt, scl_low_cnt, val;
 	u8 sda_dlyn = 0, sda_div = 0;
 
@@ -1112,6 +1113,10 @@ static void dw_hdmi_i2c_init(struct dw_hdmi_qp *hdmi)
 
 	/* Software reset */
 	hdmi_writel(hdmi, 0x01, I2CM_CONTROL0);
+
+	/* Configure I2CM hold time and rxfilter */
+	if (device_property_read_u32(hdmi->dev, "ddc-i2c-rxfilter", &ddc_i2c_rxfilter) == 0)
+		hdmi_writel(hdmi, ddc_i2c_rxfilter, I2CM_CONFIG0);
 
 	hdmi_writel(hdmi, val, I2CM_SM_SCL_CONFIG0);
 	hdmi_modb(hdmi, 0, I2CM_FM_EN, I2CM_INTERFACE_CONTROL0);
