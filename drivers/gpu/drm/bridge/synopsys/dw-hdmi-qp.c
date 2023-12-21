cd67b5961d814a9c5d9aa637466a229fa321b9eb
Joshua Riek <jjriek@verizon.net>
2023-12-21 11:40:35 -0500

    drivers: drm: dw-hdmi-qp: Add ddc-i2c-rxfilter DT prop

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi-qp.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi-qp.c
@@ -963,8 +963,14 @@ static int hdmi_bus_fmt_color_depth(unsigned int bus_format)
 
 static void dw_hdmi_i2c_init(struct dw_hdmi_qp *hdmi)
 {
+	u32 ddc_i2c_rxfilter;
+
 	/* Software reset */
 	hdmi_writel(hdmi, 0x01, I2CM_CONTROL0);
+
+	/* Configure I2CM hold time and rxfilter */
+	if (device_property_read_u32(hdmi->dev, "ddc-i2c-rxfilter", &ddc_i2c_rxfilter))
+		hdmi_writel(hdmi, ddc_i2c_rxfilter, I2CM_CONFIG0);
 
 	hdmi_modb(hdmi, 0, I2CM_FM_EN, I2CM_INTERFACE_CONTROL0);
 
