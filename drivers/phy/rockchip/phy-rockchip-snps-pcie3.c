3f5ad79191f066a5ca69da97e1617568f41f4b40
Niklas Cassel <cassel@kernel.org>
2024-04-12 14:58:16 +0200

    UPSTREAM: phy: rockchip-snps-pcie3: add support for rockchip,rx-common-refclk-mode
    >From the RK3588 Technical Reference Manual, Part1,
    section 6.19 PCIe3PHY_GRF Register Description:
    "rxX_cmn_refclk_mode"
    RX common reference clock mode for lane X. This mode should be enabled
    only when the far-end and near-end devices are running with a common
    reference clock.
    
    The hardware reset value for this field is 0x1 (enabled).
    Note that this register field is only available on RK3588, not on RK3568.
    
    The link training either fails or is highly unstable (link state will jump
    continuously between L0 and recovery) when this mode is enabled while
    using an endpoint running in Separate Reference Clock with No SSC (SRNS)
    mode or Separate Reference Clock with SSC (SRIS) mode.
    (Which is usually the case when using a real SoC as endpoint, e.g. the
    RK3588 PCIe controller can run in both Root Complex and Endpoint mode.)
    
    Add support for the device tree property rockchip,rx-common-refclk-mode,
    such that the PCIe PHY can be used in configurations where the Root
    Complex and Endpoint are not using a common reference clock.
    
    Signed-off-by: Niklas Cassel <cassel@kernel.org>
    Link: https://lore.kernel.org/r/20240412125818.17052-3-cassel@kernel.org
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Alban Browaeys <alban.browaeys@gmail.com>

--- drivers/phy/rockchip/phy-rockchip-snps-pcie3.c
+++ drivers/phy/rockchip/phy-rockchip-snps-pcie3.c
@@ -35,6 +35,12 @@
 #define RK3588_PCIE3PHY_GRF_CMN_CON0		0x0
 #define RK3588_PCIE3PHY_GRF_PHY0_STATUS1	0x904
 #define RK3588_PCIE3PHY_GRF_PHY1_STATUS1	0xa04
+#define RK3588_PCIE3PHY_GRF_PHY0_LN0_CON1	0x1004
+#define RK3588_PCIE3PHY_GRF_PHY0_LN1_CON1	0x1104
+#define RK3588_PCIE3PHY_GRF_PHY1_LN0_CON1	0x2004
+#define RK3588_PCIE3PHY_GRF_PHY1_LN1_CON1	0x2104
+#define RK3588_RX_CMN_REFCLK_MODE_EN		((BIT(7) << 16) |  BIT(7))
+#define RK3588_RX_CMN_REFCLK_MODE_DIS		(BIT(7) << 16)
 #define RK3588_SRAM_INIT_DONE(reg)		((reg & 0xf) == 0xf)
 
 /* Common definition */
@@ -57,6 +63,7 @@ struct rockchip_p3phy_priv {
 	int num_clks;
 	bool is_bifurcation;
 	bool is_initialized;
+	u32 rx_cmn_refclk_mode[4];
 };
 
 struct rockchip_p3phy_ops {
@@ -162,6 +169,19 @@ static const struct rockchip_p3phy_ops rk3568_ops = {
 
 static int rockchip_p3phy_rk3588_init(struct rockchip_p3phy_priv *priv)
 {
+	regmap_write(priv->phy_grf, RK3588_PCIE3PHY_GRF_PHY0_LN0_CON1,
+		     priv->rx_cmn_refclk_mode[0] ? RK3588_RX_CMN_REFCLK_MODE_EN :
+		     RK3588_RX_CMN_REFCLK_MODE_DIS);
+	regmap_write(priv->phy_grf, RK3588_PCIE3PHY_GRF_PHY0_LN1_CON1,
+		     priv->rx_cmn_refclk_mode[1] ? RK3588_RX_CMN_REFCLK_MODE_EN :
+		     RK3588_RX_CMN_REFCLK_MODE_DIS);
+	regmap_write(priv->phy_grf, RK3588_PCIE3PHY_GRF_PHY1_LN0_CON1,
+		     priv->rx_cmn_refclk_mode[2] ? RK3588_RX_CMN_REFCLK_MODE_EN :
+		     RK3588_RX_CMN_REFCLK_MODE_DIS);
+	regmap_write(priv->phy_grf, RK3588_PCIE3PHY_GRF_PHY1_LN1_CON1,
+		     priv->rx_cmn_refclk_mode[3] ? RK3588_RX_CMN_REFCLK_MODE_EN :
+		     RK3588_RX_CMN_REFCLK_MODE_DIS);
+
 	/* Deassert PCIe PMA output clamp mode */
 	regmap_write(priv->phy_grf, RK3588_PCIE3PHY_GRF_CMN_CON0, BIT(8) | BIT(24));
 
@@ -328,6 +348,23 @@ static int rockchip_p3phy_probe(struct platform_device *pdev)
 		};
 
 		clk_bulk_disable_unprepare(priv->num_clks, priv->clks);
+	}
+
+	ret = of_property_read_variable_u32_array(dev->of_node,
+						  "rockchip,rx-common-refclk-mode",
+						  priv->rx_cmn_refclk_mode, 1,
+						  ARRAY_SIZE(priv->rx_cmn_refclk_mode));
+	/*
+	 * if no rockchip,rx-common-refclk-mode, assume enabled for all lanes in
+	 * order to be DT backwards compatible. (Since HW reset val is enabled.)
+	 */
+	if (ret == -EINVAL) {
+		for (int i = 0; i < ARRAY_SIZE(priv->rx_cmn_refclk_mode); i++)
+			priv->rx_cmn_refclk_mode[i] = 1;
+	} else if (ret < 0) {
+		dev_err(dev, "failed to read rockchip,rx-common-refclk-mode property %d\n",
+			ret);
+		return ret;
 	}
 
 	priv->phy = devm_phy_create(dev, NULL, &rockchip_p3phy_ops);
