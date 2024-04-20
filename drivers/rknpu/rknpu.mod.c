c0fc08764a295bd5cdd324637d9d34a453874ab1
Angel-HF <49835792+Pelochus@users.noreply.github.com>
2024-04-20 15:44:47 +0200

    Updated driver to version 0.9.6

---
+++ drivers/rknpu/rknpu.mod.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>
+#define INCLUDE_VERMAGIC
+#include <linux/build-salt.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+BUILD_SALT;
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__section(".gnu.linkonce.this_module") = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+MODULE_INFO(intree, "Y");
+
+#ifdef CONFIG_RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+MODULE_INFO(depends, "");
+
+
+MODULE_INFO(srcversion, "3F8407365EF7935915D4CC3");
