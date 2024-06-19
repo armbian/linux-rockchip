dc8518f3715cd099c31937c05b2bd4915cf9fa5b
Lucas Stach <l.stach@pengutronix.de>
2024-06-19 20:21:49 +0200

    BACKPORT: drm/bridge: analogix_dp: register AUX bus after enabling runtime PM
    AUX transactions require the controller to be in working state and
    take a runtime PM reference. To avoid potential races beween the
    first transactions on the bus and runtime PM being set up, move the
    AUX registration behind the runtime PM setup.
    
    Change-Id: I36be8320c67213957c73f5b147b8fd13d1fffc41
    Signed-off-by: Lucas Stach <l.stach@pengutronix.de>
    Reviewed-by: Robert Foss <rfoss@kernel.org>
    Tested-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Robert Foss <rfoss@kernel.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240619182200.3752465-3-l.stach@pengutronix.de
    (cherry picked from commit 2d192f4a3acc1c6fe47456e13327701e62074c95)

--- drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
+++ drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
@@ -2735,6 +2735,12 @@ int analogix_dp_bind(struct analogix_dp_device *dp, struct drm_device *drm_dev)
 	dp->drm_dev = drm_dev;
 	dp->encoder = dp->plat_data->encoder;
 
+	pm_runtime_use_autosuspend(dp->dev);
+	pm_runtime_set_autosuspend_delay(dp->dev, 100);
+	pm_runtime_enable(dp->dev);
+	pm_runtime_get_sync(dp->dev);
+	analogix_dp_init(dp);
+
 	dp->aux.name = "DP-AUX";
 	dp->aux.transfer = analogix_dpaux_transfer;
 	dp->aux.dev = dp->dev;
@@ -2742,29 +2748,24 @@ int analogix_dp_bind(struct analogix_dp_device *dp, struct drm_device *drm_dev)
 
 	ret = drm_dp_aux_register(&dp->aux);
 	if (ret)
-		return ret;
-
-	pm_runtime_use_autosuspend(dp->dev);
-	pm_runtime_set_autosuspend_delay(dp->dev, 100);
-	pm_runtime_enable(dp->dev);
-	pm_runtime_get_sync(dp->dev);
-	analogix_dp_init(dp);
+		goto err_disable_pm_runtime;
 
 	ret = analogix_dp_bridge_init(dp);
 	if (ret) {
 		DRM_ERROR("failed to init bridge (%d)\n", ret);
-		goto err_disable_pm_runtime;
+		goto err_unregister_aux;
 	}
 
 	enable_irq(dp->irq);
 
 	return 0;
 
+err_unregister_aux:
+	drm_dp_aux_unregister(&dp->aux);
 err_disable_pm_runtime:
 	pm_runtime_put(dp->dev);
 	pm_runtime_dont_use_autosuspend(dp->dev);
 	pm_runtime_disable(dp->dev);
-	drm_dp_aux_unregister(&dp->aux);
 
 	return ret;
 }
