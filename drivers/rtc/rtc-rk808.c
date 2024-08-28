6bf54ea511f461b8c5075b5fdce89d618b424b34
pykpkg47 <118465017+pykpkg47@users.noreply.github.com>
2024-08-28 03:18:59 +0200

    rtc: rockchip: dts disable check add other variants
    some  boards use the rtc node of pmic to disable its rtc  , the driver checked this  only for some variants

--- drivers/rtc/rtc-rk808.c
+++ drivers/rtc/rtc-rk808.c
@@ -417,7 +417,9 @@ static int rk808_rtc_probe(struct platform_device *pdev)
 	switch (rk808->variant) {
 	case RK805_ID:
 	case RK808_ID:
+	case RK809_ID:	
 	case RK816_ID:
+	case RK817_ID:	
 	case RK818_ID:
 		np = of_get_child_by_name(pdev->dev.parent->of_node, "rtc");
 		if (np && !of_device_is_available(np)) {
