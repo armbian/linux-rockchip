3c5ef424a4f5df8b317d1fac41f2772232163d3d
amazingfate <liujianfeng1994@gmail.com>
2024-11-08 16:56:44 +0800

    PCI: rockchip: add devicetree property rockchip,default-link-up

--- drivers/pci/controller/dwc/pcie-dw-rockchip.c
+++ drivers/pci/controller/dwc/pcie-dw-rockchip.c
@@ -931,6 +931,10 @@ static const struct dw_pcie_ops dw_pcie_ops = {
 	.link_up = rk_pcie_link_up,
 };
 
+static const struct dw_pcie_ops dw_pcie_ops_default_link_up = {
+	.start_link = rk_pcie_establish_link,
+};
+
 static void rk_pcie_fast_link_setup(struct rk_pcie *rk_pcie, bool enable_dly2_en)
 {
 	u32 val;
@@ -1658,7 +1662,12 @@ static int rk_pcie_really_probe(void *p)
 	rk_pcie->msi_vector_num = data ? data->msi_vector_num : 0;
 	rk_pcie->intx = 0xffffffff;
 	pci->dev = dev;
-	pci->ops = &dw_pcie_ops;
+	if (device_property_read_bool(dev, "rockchip,default-link-up"))
+	{
+		dev_info(dev, "using pcie default link_up because of rockchip,default-link-up\n");
+		pci->ops = &dw_pcie_ops_default_link_up;
+	} else
+		pci->ops = &dw_pcie_ops;
 	platform_set_drvdata(pdev, rk_pcie);
 
 	/* 3. firmware resource */
