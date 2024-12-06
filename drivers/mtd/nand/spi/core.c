ac26ba1321ab4fcf3bc8f02012c864ea8baf68db
Jon Lin <jon.lin@rock-chips.com>
2024-12-06 10:18:14 +0800

    mtd: spinand: esmt: Support new device F50L1G41LC
    Change-Id: I87de43e56ba2936df06a89a9a0caad9ef421b652
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -1161,6 +1161,7 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 	&dosilicon_spinand_manufacturer,
 #endif
 #ifdef CONFIG_MTD_SPI_NAND_ESMT
+	&esmt_8c_spinand_manufacturer,
 	&esmt_c8_spinand_manufacturer,
 #endif
 #ifdef CONFIG_MTD_SPI_NAND_ETRON
