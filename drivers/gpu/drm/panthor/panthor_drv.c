931da4db51faad73ffeb268d26497548cf72037e
Boris Brezillon <boris.brezillon@collabora.com>
2024-12-11 09:05:00 +0100

    drm/panthor: Report innocent group kill
    Groups can be killed during a reset even though they did nothing wrong.
    That usually happens when the FW is put in a bad state by other groups,
    resulting in group suspension failures when the reset happens.
    
    If we end up in that situation, flag the group innocent and report
    innocence through a new DRM_PANTHOR_GROUP_STATE flag.
    
    Bump the minor driver version to reflect the uAPI change.
    
    Changes in v4:
    - Add an entry to the driver version changelog
    - Add R-bs
    
    Changes in v3:
    - Actually report innocence to userspace
    
    Changes in v2:
    - New patch
    
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
    Reviewed-by: Liviu Dudau <liviu.dudau@arm.com>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241211080500.2349505-1-boris.brezillon@collabora.com

--- drivers/gpu/drm/panthor/panthor_drv.c
+++ drivers/gpu/drm/panthor/panthor_drv.c
@@ -1451,6 +1451,7 @@ static const struct file_operations panthor_drm_driver_fops = {
  * - 1.1 - adds DEV_QUERY_TIMESTAMP_INFO query
  * - 1.2 - adds DEV_QUERY_GROUP_PRIORITIES_INFO query
  *       - adds PANTHOR_GROUP_PRIORITY_REALTIME priority
+ * - 1.3 - adds DRM_PANTHOR_GROUP_STATE_INNOCENT flag
  */
 static const struct drm_driver panthor_drm_driver = {
 	.driver_features = DRIVER_RENDER | DRIVER_GEM | DRIVER_SYNCOBJ |
@@ -1464,7 +1465,7 @@ static const struct drm_driver panthor_drm_driver = {
 	.desc = "Panthor DRM driver",
 	.date = "20230801",
 	.major = 1,
-	.minor = 2,
+	.minor = 3,
 
 	.gem_create_object = panthor_gem_create_object,
 	.gem_prime_import_sg_table = drm_gem_shmem_prime_import_sg_table,
