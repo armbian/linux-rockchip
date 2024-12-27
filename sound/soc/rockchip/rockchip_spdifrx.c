bb3fd4709b1f9a31de0d7c31856f6123c0471212
Zhong Shengquan <shengquan.zhong@rock-chips.com>
2024-12-27 17:31:02 +0800

    ASoC: rockchip: spdifrx: Add sample rate estimation feature
    Obtain the transmitter's sample rate through channel status bits,
    and use the CDRST register and MCLK to estimate the sample rate.
    
    Notify sync and sample rate information through controls.The control
    names are "RK SPDIFRX SYNC STATUS" and "RK SPDIFRX SAMPLE RATE". The
    application layer needs to monitor the controls and read the control
    values to obtain the information.
    
    For the sync control, 0 indicates an unlocked state, and 1 indicates
    a locked state. The sample rate control has two values, the first
    represents the sample rate obtained from the channel status, and the
    second represents the estimated sample rate.
    
    Change-Id: Ie3b4f009c55d326c4517996efa46e6e2e31c7d65
    Signed-off-by: Zhong Shengquan <shengquan.zhong@rock-chips.com>

--- sound/soc/rockchip/rockchip_spdifrx.c
+++ sound/soc/rockchip/rockchip_spdifrx.c
@@ -14,10 +14,17 @@
 #include <linux/mfd/syscon.h>
 #include <linux/regmap.h>
 #include <linux/reset.h>
+#include <sound/asoundef.h>
 #include <sound/pcm_params.h>
 #include <sound/dmaengine_pcm.h>
 
 #include "rockchip_spdifrx.h"
+
+struct rk_spdifrx_info {
+	int sync;
+	unsigned int sample_rate_src;
+	unsigned int sample_rate_cal;
+};
 
 struct rk_spdifrx_dev {
 	struct device *dev;
@@ -26,7 +33,10 @@ struct rk_spdifrx_dev {
 	struct snd_dmaengine_dai_dma_data capture_dma_data;
 	struct regmap *regmap;
 	struct reset_control *reset;
+	struct rk_spdifrx_info info;
+	struct snd_soc_dai *dai;
 	int irq;
+	bool cdr_count_avg;
 };
 
 static int rk_spdifrx_runtime_suspend(struct device *dev)
@@ -67,14 +77,22 @@ static int rk_spdifrx_hw_params(struct snd_pcm_substream *substream,
 
 	regmap_update_bits(spdifrx->regmap, SPDIFRX_INTEN,
 			   SPDIFRX_INTEN_SYNCIE_MASK |
-			   SPDIFRX_INTEN_NSYNCIE_MASK,
+			   SPDIFRX_INTEN_NSYNCIE_MASK |
+			   SPDIFRX_INTEN_BTEIE_MASK,
 			   SPDIFRX_INTEN_SYNCIE_EN |
-			   SPDIFRX_INTEN_NSYNCIE_EN);
+			   SPDIFRX_INTEN_NSYNCIE_EN |
+			   SPDIFRX_INTEN_BTEIE_EN);
 	regmap_update_bits(spdifrx->regmap, SPDIFRX_DMACR,
 			   SPDIFRX_DMACR_RDL_MASK, SPDIFRX_DMACR_RDL(8));
 	regmap_update_bits(spdifrx->regmap, SPDIFRX_CDR,
 			   SPDIFRX_CDR_AVGSEL_MASK | SPDIFRX_CDR_BYPASS_MASK,
 			   SPDIFRX_CDR_AVGSEL_MIN | SPDIFRX_CDR_BYPASS_DIS);
+
+	if (params_rate(params) >= 32000)
+		spdifrx->cdr_count_avg = true;
+	else
+		spdifrx->cdr_count_avg = false;
+
 	return 0;
 }
 
@@ -129,11 +147,76 @@ static int rk_spdifrx_trigger(struct snd_pcm_substream *substream,
 	return ret;
 }
 
+static int rk_spdifrx_sync_get(struct snd_kcontrol *kcontrol,
+			       struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_dai *dai = snd_kcontrol_chip(kcontrol);
+	struct rk_spdifrx_dev *spdifrx = snd_soc_dai_get_drvdata(dai);
+
+	ucontrol->value.integer.value[0] = spdifrx->info.sync;
+	return 0;
+}
+
+static int rk_spdifrx_sample_rate_get(struct snd_kcontrol *kcontrol,
+				      struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_dai *dai = snd_kcontrol_chip(kcontrol);
+	struct rk_spdifrx_dev *spdifrx = snd_soc_dai_get_drvdata(dai);
+
+	ucontrol->value.integer.value[0] = spdifrx->info.sample_rate_src;
+	ucontrol->value.integer.value[1] = spdifrx->info.sample_rate_cal;
+	return 0;
+}
+
+static int rk_spdifrx_sync_info(struct snd_kcontrol *kcontrol,
+				struct snd_ctl_elem_info *uinfo)
+{
+	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
+	uinfo->count = 1;
+	uinfo->value.integer.min = 0;
+	uinfo->value.integer.max = 1;
+
+	return 0;
+}
+
+static int rk_spdifrx_sample_rate_info(struct snd_kcontrol *kcontrol,
+				       struct snd_ctl_elem_info *uinfo)
+{
+	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
+	uinfo->count = 2;
+	uinfo->value.integer.min = 0;
+	uinfo->value.integer.max = 0xffffffff;
+
+	return 0;
+}
+
+static struct snd_kcontrol_new rk_spdifrx_controls[] = {
+	{
+		.iface = SNDRV_CTL_ELEM_IFACE_PCM,
+		.name = "RK SPDIFRX SYNC STATUS",
+		.access = SNDRV_CTL_ELEM_ACCESS_READ |
+			  SNDRV_CTL_ELEM_ACCESS_VOLATILE,
+		.info = rk_spdifrx_sync_info,
+		.get = rk_spdifrx_sync_get,
+	},
+	{
+		.iface = SNDRV_CTL_ELEM_IFACE_PCM,
+		.name = "RK SPDIFRX SAMPLE RATE",
+		.access = SNDRV_CTL_ELEM_ACCESS_READ |
+			  SNDRV_CTL_ELEM_ACCESS_VOLATILE,
+		.info = rk_spdifrx_sample_rate_info,
+		.get = rk_spdifrx_sample_rate_get,
+	},
+};
+
 static int rk_spdifrx_dai_probe(struct snd_soc_dai *dai)
 {
 	struct rk_spdifrx_dev *spdifrx = snd_soc_dai_get_drvdata(dai);
 
 	dai->capture_dma_data = &spdifrx->capture_dma_data;
+	spdifrx->dai = dai;
+	snd_soc_add_dai_controls(dai, rk_spdifrx_controls,
+				 ARRAY_SIZE(rk_spdifrx_controls));
 
 	return 0;
 }
@@ -149,11 +232,7 @@ static struct snd_soc_dai_driver rk_spdifrx_dai = {
 		.stream_name = "Capture",
 		.channels_min = 2,
 		.channels_max = 2,
-		.rates = (SNDRV_PCM_RATE_32000 |
-			  SNDRV_PCM_RATE_44100 |
-			  SNDRV_PCM_RATE_48000 |
-			  SNDRV_PCM_RATE_96000 |
-			  SNDRV_PCM_RATE_192000),
+		.rates = SNDRV_PCM_RATE_8000_192000,
 		.formats = (SNDRV_PCM_FMTBIT_S16_LE |
 			    SNDRV_PCM_FMTBIT_S20_3LE |
 			    SNDRV_PCM_FMTBIT_S24_LE |
@@ -181,6 +260,7 @@ static bool rk_spdifrx_wr_reg(struct device *dev, unsigned int reg)
 	case SPDIFRX_INTSR:
 	case SPDIFRX_INTCLR:
 	case SPDIFRX_SMPDR:
+	case SPDIFRX_CHNSR1:
 	case SPDIFRX_BURSTINFO:
 		return true;
 	default:
@@ -202,6 +282,7 @@ static bool rk_spdifrx_rd_reg(struct device *dev, unsigned int reg)
 	case SPDIFRX_INTSR:
 	case SPDIFRX_INTCLR:
 	case SPDIFRX_SMPDR:
+	case SPDIFRX_CHNSR1:
 	case SPDIFRX_BURSTINFO:
 		return true;
 	default:
@@ -219,6 +300,7 @@ static bool rk_spdifrx_volatile_reg(struct device *dev, unsigned int reg)
 	case SPDIFRX_INTSR:
 	case SPDIFRX_INTCLR:
 	case SPDIFRX_SMPDR:
+	case SPDIFRX_CHNSR1:
 	case SPDIFRX_BURSTINFO:
 		return true;
 	default:
@@ -248,22 +330,106 @@ static const struct regmap_config rk_spdifrx_regmap_config = {
 	.cache_type = REGCACHE_FLAT,
 };
 
+static unsigned int rk_spdifrx_get_sample_rate(unsigned int flag)
+{
+	unsigned int rate = 0;
+
+	switch (flag) {
+	case IEC958_AES3_CON_FS_22050:
+		rate = 22050;
+		break;
+	case IEC958_AES3_CON_FS_24000:
+		rate = 24000;
+		break;
+	case IEC958_AES3_CON_FS_32000:
+		rate = 32000;
+		break;
+	case IEC958_AES3_CON_FS_44100:
+		rate = 44100;
+		break;
+	case IEC958_AES3_CON_FS_48000:
+		rate = 48000;
+		break;
+	case IEC958_AES3_CON_FS_88200:
+		rate = 88200;
+		break;
+	case IEC958_AES3_CON_FS_96000:
+		rate = 96000;
+		break;
+	case IEC958_AES3_CON_FS_176400:
+		rate = 176400;
+		break;
+	case IEC958_AES3_CON_FS_192000:
+		rate = 192000;
+		break;
+	case IEC958_AES3_CON_FS_768000:
+		rate = 768000;
+		break;
+	default:
+		return 0;
+	}
+
+	return rate;
+}
+
 static irqreturn_t rk_spdifrx_isr(int irq, void *dev_id)
 {
 	struct rk_spdifrx_dev *spdifrx = dev_id;
+	struct snd_soc_dai *dai = spdifrx->dai;
+	struct snd_kcontrol *sync_kctl = snd_soc_card_get_kcontrol(dai->component->card,
+								   "RK SPDIFRX SYNC STATUS");
+	struct snd_kcontrol *sample_kctl = snd_soc_card_get_kcontrol(dai->component->card,
+								     "RK SPDIFRX SAMPLE RATE");
 	u32 intsr;
+	u32 val;
+	u32 count;
+
+	if (pm_runtime_resume_and_get(spdifrx->dev) < 0)
+		return IRQ_NONE;
 
 	regmap_read(spdifrx->regmap, SPDIFRX_INTSR, &intsr);
 
-	if (intsr & BIT(7)) {
+	if (intsr & SPDIFRX_INTSR_NSYNCISR_ACTIVE) {
+		spdifrx->info.sync = 0;
+		snd_ctl_notify(dai->component->card->snd_card,
+			       SNDRV_CTL_EVENT_MASK_VALUE, &sync_kctl->id);
 		dev_dbg(spdifrx->dev, "NSYNC\n");
-		regmap_write(spdifrx->regmap, SPDIFRX_INTCLR, BIT(7));
+		regmap_write(spdifrx->regmap, SPDIFRX_INTCLR, SPDIFRX_INTCLR_NSYNCICLR);
 	}
 
-	if (intsr & BIT(9)) {
-		dev_dbg(spdifrx->dev, "SYNC\n");
-		regmap_write(spdifrx->regmap, SPDIFRX_INTCLR, BIT(9));
+	if (intsr & SPDIFRX_INTSR_BTEISR_ACTIVE) {
+		regmap_read(spdifrx->regmap, SPDIFRX_CHNSR1, &val);
+		spdifrx->info.sample_rate_src =
+			rk_spdifrx_get_sample_rate((val & SPDIFRX_CHNSR1_SAMPLE_RATE_MASK) >> 8);
+
+		regmap_read(spdifrx->regmap, SPDIFRX_CDRST, &val);
+		if (spdifrx->cdr_count_avg)
+			count = ((val & SPDIFRX_CDRST_MINCNT_MASK) +
+				((val & SPDIFRX_CDRST_MAXCNT_MASK) >> 8)) / 4;
+		else
+			count = val & SPDIFRX_CDRST_MINCNT_MASK;
+		spdifrx->info.sample_rate_cal = clk_get_rate(spdifrx->mclk) / (count * 128);
+		snd_ctl_notify(dai->component->card->snd_card,
+			       SNDRV_CTL_EVENT_MASK_VALUE, &sample_kctl->id);
+
+		dev_dbg(spdifrx->dev, "src sample rate: %u Hz\n", spdifrx->info.sample_rate_src);
+		dev_dbg(spdifrx->dev, "cal sample rate: %u Hz\n", spdifrx->info.sample_rate_cal);
+		dev_dbg(spdifrx->dev, "BTEIE\n");
+
+		regmap_write(spdifrx->regmap, SPDIFRX_INTCLR, SPDIFRX_INTCLR_BTECLR);
+		regmap_update_bits(spdifrx->regmap, SPDIFRX_INTEN, SPDIFRX_INTEN_BTEIE_MASK,
+				   SPDIFRX_INTEN_BTEIE_DIS);
 	}
+
+	if (intsr & SPDIFRX_INTSR_SYNCISR_ACTIVE) {
+		spdifrx->info.sync = 1;
+		snd_ctl_notify(dai->component->card->snd_card,
+			       SNDRV_CTL_EVENT_MASK_VALUE, &sync_kctl->id);
+		dev_dbg(spdifrx->dev, "SYNC\n");
+		regmap_write(spdifrx->regmap, SPDIFRX_INTCLR, SPDIFRX_INTCLR_SYNCICLR);
+	}
+
+	pm_runtime_put(spdifrx->dev);
 
 	return IRQ_HANDLED;
 }
