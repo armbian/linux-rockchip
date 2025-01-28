91384396bd4a1e5e006d5ecb8b0c5af735f2f11d
Shixiong Ou <oushixiong@kylinos.cn>
2025-01-28 14:56:45 +0800

    UPSTREAM: drm/bridge: analogix_dp: Use devm_platform_ioremap_resource()
    Convert platform_get_resource(), devm_ioremap_resource() to a single
    call to devm_platform_ioremap_resource().
    
    Change-Id: I7b7610fe26f29b7d00aa4c51bce5db6d831f5cd2
    Signed-off-by: Shixiong Ou <oushixiong@kylinos.cn>
    Reviewed-by: Robert Foss <rfoss@kernel.org>
    Signed-off-by: Robert Foss <rfoss@kernel.org>
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250128065645.27140-1-oushixiong1025@163.com
    (cherry picked from commit 43c00fb1a5184ef68b1ae95a23476a85d4a4b0cc)

--- drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
+++ drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
@@ -2619,7 +2619,6 @@ analogix_dp_probe(struct device *dev, struct analogix_dp_plat_data *plat_data)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct analogix_dp_device *dp;
-	struct resource *res;
 	int ret;
 
 	if (!plat_data) {
@@ -2673,9 +2672,7 @@ analogix_dp_probe(struct device *dev, struct analogix_dp_plat_data *plat_data)
 
 	dp->nr_clks = ret;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-
-	dp->reg_base = devm_ioremap_resource(&pdev->dev, res);
+	dp->reg_base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(dp->reg_base))
 		return ERR_CAST(dp->reg_base);
 
