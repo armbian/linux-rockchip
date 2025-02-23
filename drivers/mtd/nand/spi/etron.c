5bc26fdfc67f25dd917e58ece4856c18060b2108
Jon Lin <jon.lin@rock-chips.com>
2025-02-24 00:23:13 +0800

    mtd: spinand: Etron: Support new device EM73E044VCE-H
    Change-Id: I3c9112654e8734593ac4f6af3cb1f73e0a22db35
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/etron.c
+++ drivers/mtd/nand/spi/etron.c
@@ -76,6 +76,35 @@ static int em73c044vcf_oh_ecc_get_status(struct spinand_device *spinand,
 	return -EINVAL;
 }
 
+static int em73e044vce_ooblayout_ecc(struct mtd_info *mtd, int section,
+				     struct mtd_oob_region *region)
+{
+	if (section)
+		return -ERANGE;
+
+	region->offset = mtd->oobsize / 2;
+	region->length = mtd->oobsize / 2;
+
+	return 0;
+}
+
+static int em73e044vce_ooblayout_free(struct mtd_info *mtd, int section,
+				      struct mtd_oob_region *region)
+{
+	if (section)
+		return -ERANGE;
+
+	region->offset = 2;
+	region->length = mtd->oobsize / 2 - 2;
+
+	return 0;
+}
+
+static const struct mtd_ooblayout_ops em73e044vce_ooblayout = {
+	.ecc = em73e044vce_ooblayout_ecc,
+	.free = em73e044vce_ooblayout_free,
+};
+
 static const struct spinand_info etron_spinand_table[] = {
 	SPINAND_INFO("EM73C044VCF-0H",
 		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x36),
@@ -87,6 +116,15 @@ static const struct spinand_info etron_spinand_table[] = {
 		     SPINAND_HAS_QE_BIT,
 		     SPINAND_ECCINFO(&em73c044vcf_oh_ooblayout,
 				     em73c044vcf_oh_ecc_get_status)),
+	SPINAND_INFO("EM73E044VCE-H",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x3B),
+		     NAND_MEMORG(1, 2048, 128, 64, 4096, 40, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&em73e044vce_ooblayout, em73c044vcf_oh_ecc_get_status)),
 };
 
 static const struct spinand_manufacturer_ops etron_spinand_manuf_ops = {
