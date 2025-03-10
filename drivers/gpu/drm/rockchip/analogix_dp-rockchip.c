184b3dbf5bd760b66f636a57001a04f2d43c9e37
Damon Ding <damon.ding@rock-chips.com>
2025-03-10 18:41:09 +0800

    BACKPORT: drm/rockchip: analogix_dp: Add support to get panel from the DP AUX bus
    Move drm_of_find_panel_or_bridge() a little later and combine it with
    component_add() into a new function rockchip_dp_link_panel(). The function
    will serve as done_probing() callback of devm_of_dp_aux_populate_bus(),
    aiding to support for obtaining the eDP panel via the DP AUX bus.
    
    If failed to get the panel from the DP AUX bus, it will then try the other
    way to get panel information through the platform bus.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Change-Id: I04dfdd61a9df263375ae7af05e8bda08470713f8
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Link: https://lore.kernel.org/r/20250310104114.2608063-9-damon.ding@rock-chips.com
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    (cherry picked from commit d7b4936b2bc0987ccea125d9653381a1a0038d6d)

--- drivers/gpu/drm/rockchip/analogix_dp-rockchip.c
+++ drivers/gpu/drm/rockchip/analogix_dp-rockchip.c
@@ -22,6 +22,7 @@
 #include <video/of_videomode.h>
 #include <video/videomode.h>
 
+#include <drm/display/drm_dp_aux_bus.h>
 #include <drm/display/drm_dp_helper.h>
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
@@ -761,22 +762,37 @@ static const struct component_ops rockchip_dp_component_ops = {
 	.unbind = rockchip_dp_unbind,
 };
 
+static int rockchip_dp_link_panel(struct drm_dp_aux *aux)
+{
+	struct analogix_dp_plat_data *plat_data = analogix_dp_aux_to_plat_data(aux);
+	struct rockchip_dp_device *dp = pdata_encoder_to_dp(plat_data);
+	int ret;
+
+	/*
+	 * If drm_of_find_panel_or_bridge() returns -ENODEV, there may be no valid panel
+	 * or bridge nodes. The driver should go on for the driver-free bridge or the DP
+	 * mode applications.
+	 */
+	ret = drm_of_find_panel_or_bridge(dp->dev->of_node, 1, 0,
+					  &plat_data->panel, &plat_data->bridge);
+	if (ret && ret != -ENODEV)
+		return ret;
+
+	dp->plat_data.skip_connector = rockchip_dp_skip_connector(dp->plat_data.bridge);
+
+	return component_add(dp->dev, &rockchip_dp_component_ops);
+}
+
 static int rockchip_dp_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	const struct rockchip_dp_chip_data *dp_data;
-	struct drm_panel *panel = NULL;
-	struct drm_bridge *bridge = NULL;
 	struct rockchip_dp_device *dp;
 	int id, i, ret;
 
 	dp_data = of_device_get_match_data(dev);
 	if (!dp_data)
 		return -ENODEV;
-
-	ret = drm_of_find_panel_or_bridge(dev->of_node, 1, 0, &panel, &bridge);
-	if (ret < 0 && ret != -ENODEV)
-		return ret;
 
 	dp = devm_kzalloc(dev, sizeof(*dp), GFP_KERNEL);
 	if (!dp)
@@ -802,7 +818,6 @@ static int rockchip_dp_probe(struct platform_device *pdev)
 	dp->plat_data.ssc = dp->data->ssc;
 	dp->plat_data.support_dp_mode = dp->data->support_dp_mode;
 	dp->plat_data.max_bpc = dp->data->max_bpc ? dp->data->max_bpc : 8;
-	dp->plat_data.panel = panel;
 	dp->plat_data.dev_type = dp->data->chip_type;
 	dp->plat_data.power_on = rockchip_dp_poweron;
 	dp->plat_data.power_off = rockchip_dp_powerdown;
@@ -811,8 +826,6 @@ static int rockchip_dp_probe(struct platform_device *pdev)
 	dp->plat_data.detach = rockchip_dp_bridge_detach;
 	dp->plat_data.convert_to_split_mode = drm_mode_convert_to_split_mode;
 	dp->plat_data.convert_to_origin_mode = drm_mode_convert_to_origin_mode;
-	dp->plat_data.skip_connector = rockchip_dp_skip_connector(bridge);
-	dp->plat_data.bridge = bridge;
 	dp->plat_data.disable_psr = device_property_read_bool(dp->dev, "rockchip,disable-psr");
 
 	ret = rockchip_dp_of_probe(dp);
@@ -856,9 +869,20 @@ static int rockchip_dp_probe(struct platform_device *pdev)
 			dp->plat_data.left_display = true;
 	}
 
-	ret = component_add(dev, &rockchip_dp_component_ops);
-	if (ret)
-		return ret;
+	ret = devm_of_dp_aux_populate_bus(analogix_dp_get_aux(dp->adp), rockchip_dp_link_panel);
+	if (ret) {
+		/*
+		 * If devm_of_dp_aux_populate_bus() returns -ENODEV, the done_probing() will not
+		 * be called because there are no EP devices. Then the rockchip_dp_link_panel()
+		 * will be called directly in order to support the other valid DT configurations.
+		 *
+		 * NOTE: The devm_of_dp_aux_populate_bus() is allowed to return -EPROBE_DEFER.
+		 */
+		if (ret != -ENODEV)
+			return dev_err_probe(dp->dev, ret, "failed to populate aux bus\n");
+
+		return rockchip_dp_link_panel(analogix_dp_get_aux(dp->adp));
+	}
 
 	return 0;
 }
