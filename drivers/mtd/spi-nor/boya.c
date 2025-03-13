2302af2d8f4443ca3a00d369ab6419366ed8b867
Jon Lin <jon.lin@rock-chips.com>
2025-03-13 12:03:57 +0800

    mtd: spi-nor: boya: convert flash_info to new format
    Change-Id: I2f3afc3705c51aa9b941a7247e0a92b983df105a
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi-nor/boya.c
+++ drivers/mtd/spi-nor/boya.c
@@ -8,11 +8,18 @@
 #include "core.h"
 
 static const struct flash_info boya_parts[] = {
-	{ "BY25Q256FSEIG", INFO(0x684919, 0, 64 * 1024, 512)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
-		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
-	{ "BY25Q64ESSIG", INFO(0x684017, 0, 64 * 1024, 128)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ) },
+	{
+		.id = SNOR_ID(0x68, 0x49, 0x19),
+		.name = "BY25Q256FSEIG",
+		.size = SZ_32M,
+		.flags = SPI_NOR_4B_OPCODES,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	}, {
+		.id = SNOR_ID(0x68, 0x40, 0x17),
+		.name = "BY25Q64ESSIG",
+		.size = SZ_8M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	},
 };
 
 const struct spi_nor_manufacturer spi_nor_boya = {
