d5dbb6bcb3297ead172807273e1b8eb9b5bc01fe
Jon Lin <jon.lin@rock-chips.com>
2025-03-13 12:03:11 +0800

    mtd: spi-nor: fmsh: convert flash_info to new format
    Change-Id: Ifc3d29d761dd80dee8fa0a6920f3c1ee182f62e9
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi-nor/fmsh.c
+++ drivers/mtd/spi-nor/fmsh.c
@@ -8,15 +8,23 @@
 #include "core.h"
 
 static const struct flash_info fmsh_parts[] = {
-	{ "FM25Q64A", INFO(0xA14017, 0, 64 * 1024, 128)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ |
-			      SPI_NOR_QUAD_READ) },
-	{ "FM25Q128A", INFO(0xA14018, 0, 64 * 1024, 256)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ |
-			      SPI_NOR_QUAD_READ) },
-	{ "FM25Q256I3", INFO(0xA14019, 0, 64 * 1024, 512)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
-		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
+	{
+		.id = SNOR_ID(0xA1, 0x40, 0x17),
+		.name = "FM25Q64A",
+		.size = SZ_8M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	}, {
+		.id = SNOR_ID(0xA1, 0x40, 0x18),
+		.name = "FM25Q128A",
+		.size = SZ_16M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	}, {
+		.id = SNOR_ID(0xA1, 0x40, 0x19),
+		.name = "FM25Q256I3",
+		.size = SZ_32M,
+		.flags = SPI_NOR_4B_OPCODES,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	},
 };
 
 const struct spi_nor_manufacturer spi_nor_fmsh = {
