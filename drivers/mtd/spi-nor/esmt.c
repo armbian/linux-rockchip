c911d0c1fa8ad5e6b2d47c714fd6cfedd5d349a0
Jon Lin <jon.lin@rock-chips.com>
2025-03-18 22:01:29 +0800

    mtd: spi-nor: esmt: convert flash_info to new format
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>
    Change-Id: Idf61deb80336ed2bab2e29058c3e7873ff171593

--- drivers/mtd/spi-nor/esmt.c
+++ drivers/mtd/spi-nor/esmt.c
@@ -27,33 +27,39 @@ static const struct flash_info esmt_nor_parts[] = {
 		.size = SZ_8M,
 		.flags = SPI_NOR_HAS_LOCK,
 		.no_sfdp_flags = SECT_4K,
+	}, {
+		.id = SNOR_ID(0x1c, 0x70, 0x18),
+		.name = "en25qh128",
+		.size = SZ_16M,
+		.no_sfdp_flags = SECT_4K,
+	}, {
+		.id = SNOR_ID(0x1c, 0x38, 0x17),
+		.name = "en25s64",
+		.size = SZ_8M,
+		.no_sfdp_flags = SECT_4K,
+	}, {
+		.id = SNOR_ID(0x1c, 0x70, 0x19),
+		.name = "en25qh256a",
+		.size = SZ_32M,
+		.flags = SPI_NOR_4B_OPCODES,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	}, {
+		.id = SNOR_ID(0x1c, 0x71, 0x19),
+		.name = "en25qx256a",
+		.size = SZ_32M,
+		.flags = SPI_NOR_4B_OPCODES,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	}, {
+		.id = SNOR_ID(0x1c, 0x71, 0x18),
+		.name = "en25qx128a",
+		.size = SZ_16M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
+	}, {
+		.id = SNOR_ID(0x1c, 0x71, 0x17),
+		.name = "en25qx64a",
+		.size = SZ_8M,
+		.no_sfdp_flags = SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ,
 	}
-	/* ESMT */
-	{ "f25l32pa", INFO(0x8c2016, 0, 64 * 1024, 64)
-		FLAGS(SPI_NOR_HAS_LOCK | SPI_NOR_SWP_IS_VOLATILE)
-		NO_SFDP_FLAGS(SECT_4K) },
-	{ "f25l32qa-2s", INFO(0x8c4116, 0, 64 * 1024, 64)
-		FLAGS(SPI_NOR_HAS_LOCK)
-		NO_SFDP_FLAGS(SECT_4K) },
-	{ "f25l64qa", INFO(0x8c4117, 0, 64 * 1024, 128)
-		FLAGS(SPI_NOR_HAS_LOCK)
-		NO_SFDP_FLAGS(SECT_4K) },
-	{ "en25qh128", INFO(0x1c7018, 0, 64 * 1024, 256)
-		NO_SFDP_FLAGS(SECT_4K) },
-	{ "en25s64", INFO(0x1c3817, 0, 64 * 1024, 128)
-		NO_SFDP_FLAGS(SECT_4K) },
-	{ "en25qh256a", INFO(0x1c7019, 0, 64 * 1024, 512)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
-		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
-	{ "en25qx256a", INFO(0x1c7119, 0, 64 * 1024, 512)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ)
-		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
-	{ "en25qx128a", INFO(0x1c7118, 0, 64 * 1024, 256)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ |
-			      SPI_NOR_QUAD_READ) },
-	{ "en25qx64a", INFO(0x1c7117, 0, 64 * 1024, 128)
-		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ |
-			      SPI_NOR_QUAD_READ) },
 };
 
 const struct spi_nor_manufacturer spi_nor_esmt = {
