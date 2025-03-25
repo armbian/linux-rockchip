f919610bcbd1cd10002dd2a27387607f67f35c95
Damon Ding <damon.ding@rock-chips.com>
2025-03-25 16:45:04 +0800

    drm/rockchip: lvds: include <linux/platform_device.h>
    Change-Id: I4b9febaaa59e2862f26d6262e2ad765f8330a5f6
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_lvds.c
+++ drivers/gpu/drm/rockchip/rockchip_lvds.c
@@ -10,7 +10,7 @@
 #include <linux/mfd/syscon.h>
 #include <linux/of_graph.h>
 #include <linux/phy/phy.h>
-#include <linux/of_platform.h>
+#include <linux/platform_device.h>
 #include <linux/regmap.h>
 #include <linux/reset.h>
 
