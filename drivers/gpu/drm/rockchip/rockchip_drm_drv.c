4fbcce6cbbe9405cad70afde3fdb031c147af986
Tao Huang <huangtao@rock-chips.com>
2025-05-20 10:17:28 +0800

    drm/rockchip: drv: Add a gfp parameter to iommu_map()
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: Iaf9be2344ff06cc33e2bd081e36e17d715b70f00

--- drivers/gpu/drm/rockchip/rockchip_drm_drv.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_drv.c
@@ -1419,14 +1419,14 @@ static int rockchip_drm_init_iommu(struct drm_device *drm_dev)
 		 * cliped to 0, so we split into two mapping
 		 */
 		ret = iommu_map(private->domain, 0, 0, (size_t)SZ_2G,
-				IOMMU_WRITE | IOMMU_READ | IOMMU_PRIV);
+				IOMMU_WRITE | IOMMU_READ | IOMMU_PRIV, GFP_KERNEL);
 		if (ret) {
 			dev_err(drm_dev->dev, "failed to create 0-2G pre mapping\n");
 			return 0;
 		}
 
 		ret = iommu_map(private->domain, SZ_2G, SZ_2G, (size_t)SZ_2G,
-				IOMMU_WRITE | IOMMU_READ | IOMMU_PRIV);
+				IOMMU_WRITE | IOMMU_READ | IOMMU_PRIV, GFP_KERNEL);
 		if (ret) {
 			dev_err(drm_dev->dev, "failed to create 2G-4G pre mapping\n");
 			return 0;
