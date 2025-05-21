228d4bc281d913c6c1e7402641e2f79ccb583418
Tao Huang <huangtao@rock-chips.com>
2025-05-21 17:05:40 +0800

    drm/rockchip: rk618: Explicitly include correct DT includes
    Change-Id: Id51afc51e26e73245fad6dbeccf06f0c0dd473c4
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/gpu/drm/rockchip/rk618/rk618_scaler.c
+++ drivers/gpu/drm/rockchip/rk618/rk618_scaler.c
@@ -11,7 +11,8 @@
 #include <linux/mfd/rk618.h>
 #include <linux/mfd/syscon.h>
 #include <linux/module.h>
-#include <linux/of_device.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
 #include <linux/regmap.h>
 
 #include <drm/drm_of.h>
