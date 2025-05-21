7365643654b4e481aa80b1807cf4a5db45903816
Tao Huang <huangtao@rock-chips.com>
2025-05-21 18:51:30 +0800

    net: can: rockchip: rk3562: Make remove callback return void
    Change-Id: I3e08920310d490585619e38d88b1098ff5194597
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/net/can/rockchip/rk3562_can.c
+++ drivers/net/can/rockchip/rk3562_can.c
@@ -1041,7 +1041,7 @@ err_free_candev:
 	return err;
 }
 
-static int rk3562_can_remove(struct platform_device *pdev)
+static void rk3562_can_remove(struct platform_device *pdev)
 {
 	struct net_device *ndev = platform_get_drvdata(pdev);
 	struct rk3562_can *rcan = netdev_priv(ndev);
@@ -1050,8 +1050,6 @@ static int rk3562_can_remove(struct platform_device *pdev)
 	pm_runtime_disable(&pdev->dev);
 	netif_napi_del(&rcan->napi);
 	free_candev(ndev);
-
-	return 0;
 }
 
 static struct platform_driver rk3562_can_driver = {
