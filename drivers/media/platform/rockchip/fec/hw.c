02d376d92178abdc01953eee068418a525737e22
Tao Huang <huangtao@rock-chips.com>
2025-05-25 19:39:29 +0800

    media: rockchip: fec: Make remove callback return void
    Change-Id: I4625fa922f8f993e0be5bb99ab3d761030ce7a03
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/media/platform/rockchip/fec/hw.c
+++ drivers/media/platform/rockchip/fec/hw.c
@@ -297,14 +297,13 @@ err:
 	return ret;
 }
 
-static int rkfec_hw_remove(struct platform_device *pdev)
+static void rkfec_hw_remove(struct platform_device *pdev)
 {
 	struct rkfec_hw_dev *hw_dev = platform_get_drvdata(pdev);
 
 	rkfec_unregister_offline(hw_dev);
 	pm_runtime_disable(&pdev->dev);
 	mutex_destroy(&hw_dev->dev_lock);
-	return 0;
 }
 
 static void rkfec_hw_shutdown(struct platform_device *pdev)
