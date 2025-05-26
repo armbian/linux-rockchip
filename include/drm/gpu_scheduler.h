9af001b24d7bada32cdafa496b609956d2a5b9b6
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2025-05-26 14:54:44 +0200

    drm/sched: Store the drm client_id in drm_sched_fence
    This will be used in a later commit to trace the drm client_id in
    some of the gpu_scheduler trace events.
    
    This requires changing all the users of drm_sched_job_init to
    add an extra parameter.
    
    The newly added drm_client_id field in the drm_sched_fence is a bit
    of a duplicate of the owner one. One suggestion I received was to
    merge those 2 fields - this can't be done right now as amdgpu uses
    some special values (AMDGPU_FENCE_OWNER_*) that can't really be
    translated into a client id. Christian is working on getting rid of
    those; when it's done we should be able to squash owner/drm_client_id
    together.
    
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
    Signed-off-by: Philipp Stanner <phasta@kernel.org>
    Link: https://lore.kernel.org/r/20250526125505.2360-3-pierre-eric.pelloux-prayer@amd.com

--- include/drm/gpu_scheduler.h
+++ include/drm/gpu_scheduler.h
@@ -303,6 +303,13 @@ struct drm_sched_fence {
          * @owner: job owner for debugging
          */
 	void				*owner;
+
+	/**
+	 * @drm_client_id:
+	 *
+	 * The client_id of the drm_file which owns the job.
+	 */
+	uint64_t			drm_client_id;
 };
 
 struct drm_sched_fence *to_drm_sched_fence(struct dma_fence *f);
@@ -568,7 +575,8 @@ int drm_sched_init(struct drm_gpu_scheduler *sched,
 void drm_sched_fini(struct drm_gpu_scheduler *sched);
 int drm_sched_job_init(struct drm_sched_job *job,
 		       struct drm_sched_entity *entity,
-		       u32 credits, void *owner);
+		       u32 credits, void *owner,
+		       u64 drm_client_id);
 void drm_sched_job_arm(struct drm_sched_job *job);
 int drm_sched_job_add_dependency(struct drm_sched_job *job,
 				 struct dma_fence *fence);
