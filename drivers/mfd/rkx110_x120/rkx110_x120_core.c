3929dd5026fb73f14ec78b238c8c14c9675ac435
Tao Huang <huangtao@rock-chips.com>
2025-05-26 17:38:35 +0800

    mfd: rkx110_x120: Drop the unused id parameter from rk_serdes_i2c_probe()
    Change-Id: I57e791859355a02b832c83307f3d224458157bfe
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/mfd/rkx110_x120/rkx110_x120_core.c
+++ drivers/mfd/rkx110_x120/rkx110_x120_core.c
@@ -872,7 +872,7 @@ static irqreturn_t rk_serdes_irq_handler(int irq, void *arg)
 	return IRQ_HANDLED;
 }
 
-static int rk_serdes_i2c_probe(struct i2c_client *client, const struct i2c_device_id *id)
+static int rk_serdes_i2c_probe(struct i2c_client *client)
 {
 	struct device *dev = &client->dev;
 	struct device_node *disp_np;
