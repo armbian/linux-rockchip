2b3cf52f363878f701ce93a80fcb500fff530c59
Tao Huang <huangtao@rock-chips.com>
2025-05-26 19:30:52 +0800

    media: i2c: gc05a2: Drop the unused id parameter from gc05a2_probe()
    Change-Id: I11bb25bce683c22d8b693bcbf0dfe3afa27ab29f
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/media/i2c/gc05a2.c
+++ drivers/media/i2c/gc05a2.c
@@ -1585,8 +1585,7 @@ static int gc05a2_parse_of(struct gc05a2 *gc05a2)
 	return 0;
 }
 
-static int gc05a2_probe(struct i2c_client *client,
-			 const struct i2c_device_id *id)
+static int gc05a2_probe(struct i2c_client *client)
 {
 	struct device *dev = &client->dev;
 	struct device_node *node = dev->of_node;
