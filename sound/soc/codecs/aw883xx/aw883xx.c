f0fada0195dbf6c55104abbad9e1ee1a85cf247b
Tao Huang <huangtao@rock-chips.com>
2025-05-26 14:35:13 +0800

    ASoC: aw883xx: Drop the unused id parameter from aw883xx_i2c_probe()
    Change-Id: I9a8d69da54199ebe943514e42ec3862af74bac71
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- sound/soc/codecs/aw883xx/aw883xx.c
+++ sound/soc/codecs/aw883xx/aw883xx.c
@@ -2405,8 +2405,7 @@ static struct attribute_group aw883xx_attribute_group = {
  * i2c driver
  *
  ******************************************************/
-static int aw883xx_i2c_probe(struct i2c_client *i2c,
-				const struct i2c_device_id *id)
+static int aw883xx_i2c_probe(struct i2c_client *i2c)
 {
 	struct aw883xx *aw883xx = NULL;
 	int ret = -1;
