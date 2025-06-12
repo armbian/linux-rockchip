502e2c1f38bd79d47ab3ccd8e03d99fcb5f365f7
Tao Huang <huangtao@rock-chips.com>
2025-06-12 20:20:29 +0800

    net: wireless: rockchip_wlan: bcmdhd: Fix compilation errors on kernel 6.12
    Change-Id: I3cf501f5b8a0a5e61f496ab8b239a372d878d485
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_cfgvif.c
+++ drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_cfgvif.c
@@ -5987,8 +5987,9 @@ wl_cfg80211_ch_switch_notify(struct net_device *dev, uint16 chanspec, struct wip
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION (3, 8, 0))
 	freq = chandef.chan ? chandef.chan->center_freq : chandef.center_freq1;
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0) || \
-		((ANDROID_VERSION >= 13) && (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 94)))
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 9, 0) && \
+	(LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0) || \
+		((ANDROID_VERSION >= 13) && (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 94))))
 	cfg80211_ch_switch_notify(dev, &chandef, 0, 0);
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 19, 2) || defined(CFG80211_BKPORT_MLO)
 	cfg80211_ch_switch_notify(dev, &chandef, 0);
