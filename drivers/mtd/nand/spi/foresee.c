531f8a92456c5c7b072c1051c744f03895b3a866
Jon Lin <jon.lin@rock-chips.com>
2025-06-18 10:24:59 +0800

    mtd: spinand: foresee: Support new device F35SQB001G
    Change-Id: I76fcc31b67e521aba6f422628413fee3e3d37ebd
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/foresee.c
+++ drivers/mtd/nand/spi/foresee.c
@@ -202,6 +202,15 @@ static const struct spinand_info foresee_spinand_table[] = {
 					      &update_cache_variants),
 		     SPINAND_HAS_QE_BIT,
 		     SPINAND_ECCINFO(&fsxxndxxg_ooblayout, f35sqb00xg_ecc_get_status)),
+	SPINAND_INFO("F35SQB001G",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0x71),
+		     NAND_MEMORG(1, 2048, 128, 64, 1024, 20, 1, 1, 1),
+		     NAND_ECCREQ(1, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&f35sqb00xg_ooblayout, NULL)),
 };
 
 static const struct spinand_manufacturer_ops foresee_spinand_manuf_ops = {
