f376a80ae956301f31ef69260b284bb111ac2c79
Cai YiWei <cyw@rock-chips.com>
2025-08-01 14:15:50 +0800

    media: rockchip: isp: version v3.1.0
    Change-Id: I59b1c6ce390e377724b68488bef4066b23b45fad
    Signed-off-by: Cai YiWei <cyw@rock-chips.com>

--- include/uapi/linux/rk-isp2-config.h
+++ include/uapi/linux/rk-isp2-config.h
@@ -12,7 +12,7 @@
 #include <linux/v4l2-controls.h>
 #include <linux/rk-camera-module.h>
 
-#define RKISP_API_VERSION		KERNEL_VERSION(3, 0, 0)
+#define RKISP_API_VERSION		KERNEL_VERSION(3, 1, 0)
 
 /****************ISP SUBDEV IOCTL*****************************/
 
