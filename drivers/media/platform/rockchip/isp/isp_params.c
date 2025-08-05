2d9deb5480c8979b759b4d327ec4e3dfa7d93409
Cai YiWei <cyw@rock-chips.com>
2025-08-05 16:09:01 +0800

    media: rockchip: isp: clear aiisp l2 stop status
    Change-Id: I29dbdd8e4790bd58e3c87472a6faf25d58f8986e
    Signed-off-by: Cai YiWei <cyw@rock-chips.com>

--- drivers/media/platform/rockchip/isp/isp_params.c
+++ drivers/media/platform/rockchip/isp/isp_params.c
@@ -327,6 +327,7 @@ static void rkisp_params_vb2_stop_streaming(struct vb2_queue *vq)
 	dev->is_aiisp_l2_st = false;
 	dev->is_aiisp_l2_first_cfg = false;
 	dev->is_aiisp_l2_init = false;
+	dev->is_aiisp_l2_waiting = false;
 	dev->is_aiisp_stop = false;
 	dev->is_aiisp_stopping = false;
 	memset(&dev->aiisp_cfg, 0, sizeof(dev->aiisp_cfg));
