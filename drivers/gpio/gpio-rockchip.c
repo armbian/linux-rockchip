173e365f42b5bd1cca644545312af9c8dd555bd6
Ye Zhang <ye.zhang@rock-chips.com>
2025-10-09 15:02:30 +0800

    gpio: rockchip: Update GPIO_TYPE_V2_6 and GPIO_TYPE_V2_6_1
    Signed-off-by: Ye Zhang <ye.zhang@rock-chips.com>
    Change-Id: I7f54ec5e74c9124dfda726fbb7ccc97d6b9317aa

--- drivers/gpio/gpio-rockchip.c
+++ drivers/gpio/gpio-rockchip.c
@@ -34,7 +34,8 @@
 #define GPIO_TYPE_V2		(0x01000C2B)  /* GPIO Version ID 0x01000C2B */
 #define GPIO_TYPE_V2_1		(0x0101157C)  /* GPIO Version ID 0x0101157C */
 #define GPIO_TYPE_V2_2		(0x010219C8)  /* GPIO Version ID 0x010219C8 */
-#define GPIO_TYPE_V2_6		(0x01063F6E)  /* GPIO Version ID 0x01063F6E */
+#define GPIO_TYPE_V2_6		(0x010637A6)  /* GPIO Version ID 0x010637A6 */
+#define GPIO_TYPE_V2_6_1	(0x01063F6E)  /* GPIO Version ID 0x01063F6E */
 
 #define GPIO_MAX_PINS	(32)
 
@@ -799,6 +800,7 @@ static void rockchip_gpio_get_ver(struct rockchip_pin_bank *bank)
 		bank->gpio_type = GPIO_TYPE_V2_2;
 		break;
 	case GPIO_TYPE_V2_6:
+	case GPIO_TYPE_V2_6_1:
 		bank->gpio_regs = &gpio_regs_v2;
 		bank->gpio_type = GPIO_TYPE_V2_6;
 		break;
