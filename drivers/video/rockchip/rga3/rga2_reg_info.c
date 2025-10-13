a79e7a629c144bc2d6403efae385b25df62a1ac0
Yu Qiaowei <cerf.yu@rock-chips.com>
2025-10-13 20:54:49 +0800

    video: rockchip: rga3: support input RGBA/BGRA5551 RGBA/BGRA4444
    Change-Id: I8c7572617b2a1fb4ae1889d553cd1c150673e72c
    Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>

--- drivers/video/rockchip/rga3/rga2_reg_info.c
+++ drivers/video/rockchip/rga3/rga2_reg_info.c
@@ -527,6 +527,28 @@ static void RGA2_set_reg_src_info(u8 *base, struct rga2_req *msg)
 		msg->src_trans_mode &= 0x07;
 		src0_rb_swp = 0x1;
 		break;
+	case RGA_FORMAT_RGBA_5551:
+		src0_format = 0x5;
+		pixel_width = 2;
+		src0_alpha_swp = 1;
+		break;
+	case RGA_FORMAT_BGRA_5551:
+		src0_format = 0x5;
+		pixel_width = 2;
+		src0_rb_swp = 0x1;
+		src0_alpha_swp = 1;
+		break;
+	case RGA_FORMAT_RGBA_4444:
+		src0_format = 0x6;
+		pixel_width = 2;
+		src0_alpha_swp = 1;
+		break;
+	case RGA_FORMAT_BGRA_4444:
+		src0_format = 0x6;
+		pixel_width = 2;
+		src0_rb_swp = 0x1;
+		src0_alpha_swp = 1;
+		break;
 
 		/* ARGB */
 		/*
@@ -1058,6 +1080,28 @@ static void RGA2_set_reg_dst_info(u8 *base, struct rga2_req *msg)
 		src1_format = 0x4;
 		spw = 2;
 		src1_rb_swp = 0x1;
+		break;
+	case RGA_FORMAT_RGBA_5551:
+		src1_format = 0x5;
+		spw = 2;
+		src1_alpha_swp = 1;
+		break;
+	case RGA_FORMAT_BGRA_5551:
+		src1_format = 0x5;
+		spw = 2;
+		src1_rb_swp = 0x1;
+		src1_alpha_swp = 1;
+		break;
+	case RGA_FORMAT_RGBA_4444:
+		src1_format = 0x6;
+		spw = 2;
+		src1_alpha_swp = 1;
+		break;
+	case RGA_FORMAT_BGRA_4444:
+		src1_format = 0x6;
+		spw = 2;
+		src1_rb_swp = 0x1;
+		src1_alpha_swp = 1;
 		break;
 
 		/* ARGB */
