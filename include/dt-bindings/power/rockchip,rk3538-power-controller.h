caac24827f893c76e19b217252ca92eb92e3aaf2
Finley Xiao <finley.xiao@rock-chips.com>
2025-11-06 19:24:43 +0800

    dt-bindings: power: rockchip: Add support for RK3538
    Add power domain IDs for RK3535 SoC.
    Add a new compatible because register fields have changed.
    
    Change-Id: Ic934a023b2e3211fa6f6de5202a75f7e06ca1d66
    Signed-off-by: Finley Xiao <finley.xiao@rock-chips.com>

---
+++ include/dt-bindings/power/rockchip,rk3538-power-controller.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+/*
+ * Copyright (c) 2025 Rockchip Electronics Co., Ltd.
+ * Author: Finley Xiao <finley.xiao@rock-chips.com>
+ */
+
+#ifndef __DT_BINDINGS_POWER_RK3538_POWER_CONTROLLER_H__
+#define __DT_BINDINGS_POWER_RK3538_POWER_CONTROLLER_H__
+
+/* VD_LOGIC */
+#define RK3538_PD_RKVDEC	0
+#define RK3538_PD_PHPL		1
+#define RK3538_PD_PHPR		2
+#define RK3538_PD_VPU		3
+#define RK3538_PD_VO		4
+#define RK3538_PD_GPU		5
+
+#endif
