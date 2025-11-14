e41d79670638f8627e6853ff0c9193bd16b33786
Algea Cao <algea.cao@rock-chips.com>
2025-11-14 09:52:49 +0800

    drm/rockchip: vop2: Support rk3538 hdmi yuv422 output
    Change-Id: Id0dcc94de6ca08a421d54ef35db6630104eedecf
    Signed-off-by: Algea Cao <algea.cao@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -11622,7 +11622,8 @@ static void vop2_crtc_setup_output_mode(struct drm_crtc *crtc)
 		else if (vop2->version == VOP_VERSION_RK3588 &&
 			 output_if_is_edp(vcstate->output_if))
 			out_mode = RK3588_EDP_OUTPUT_MODE_YUV422;
-		else if (vop2->version == VOP_VERSION_RK3576 &&
+		else if ((vop2->version == VOP_VERSION_RK3576 ||
+			  vop2->version == VOP_VERSION_RK3538) &&
 			 output_if_is_hdmi(vcstate->output_if))
 			out_mode = RK3576_HDMI_OUT_MODE_YUV422;
 		else if (output_if_is_dp(vcstate->output_if))
