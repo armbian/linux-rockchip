torvalds/linux

commit 7291693b01c8cf9c278621e7cb766dd05ea2cec5

video: rockchip: rga3: compatible with legacy global alpha config
Change-Id: I8dcccad5638f1fa7f16dcd9945562596cae047bc
Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>

drivers/video/rockchip/rga3/rga2_reg_info.c
@@ -2087,19 +2087,26 @@ static void rga_cmd_to_rga2_cmd(struct rga_scheduler_t *scheduler,
 				req->alpha_config.bg_pixel_alpha_en =
 					rga_is_alpha_format(req->dst.format);
 
-			if (req_rga->fg_global_alpha < 0xff) {
-				req->alpha_config.fg_global_alpha_en = true;
-				req->alpha_config.fg_global_alpha_value = req_rga->fg_global_alpha;
-			} else if (!req->alpha_config.fg_pixel_alpha_en) {
-				req->alpha_config.fg_global_alpha_en = true;
-				req->alpha_config.fg_global_alpha_value = 0xff;
-			}
+			if (req_rga->feature.global_alpha_en) {
+				if (req_rga->fg_global_alpha < 0xff) {
+					req->alpha_config.fg_global_alpha_en = true;
+					req->alpha_config.fg_global_alpha_value =
+						req_rga->fg_global_alpha;
+				} else if (!req->alpha_config.fg_pixel_alpha_en) {
+					req->alpha_config.fg_global_alpha_en = true;
+					req->alpha_config.fg_global_alpha_value = 0xff;
+				}
 
-			if (req_rga->bg_global_alpha < 0xff) {
-				req->alpha_config.bg_global_alpha_en = true;
-				req->alpha_config.bg_global_alpha_value = req_rga->bg_global_alpha;
-			} else if (!req->alpha_config.bg_pixel_alpha_en) {
-				req->alpha_config.bg_global_alpha_en = true;
+				if (req_rga->bg_global_alpha < 0xff) {
+					req->alpha_config.bg_global_alpha_en = true;
+					req->alpha_config.bg_global_alpha_value =
+						req_rga->bg_global_alpha;
+				} else if (!req->alpha_config.bg_pixel_alpha_en) {
+					req->alpha_config.bg_global_alpha_en = true;
+					req->alpha_config.bg_global_alpha_value = 0xff;
+				}
+			} else {
+				req->alpha_config.bg_global_alpha_value = 0xff;
 				req->alpha_config.bg_global_alpha_value = 0xff;
 			}
 

drivers/video/rockchip/rga3/rga3_reg_info.c
@@ -1643,19 +1643,26 @@ static void rga_cmd_to_rga3_cmd(struct rga_req *req_rga, struct rga3_req *req)
 			req->alpha_config.fg_pixel_alpha_en = rga_is_alpha_format(req->win1.format);
 			req->alpha_config.bg_pixel_alpha_en = rga_is_alpha_format(req->win0.format);
 
-			if (req_rga->fg_global_alpha < 0xff) {
-				req->alpha_config.fg_global_alpha_en = true;
-				req->alpha_config.fg_global_alpha_value = req_rga->fg_global_alpha;
-			} else if (!req->alpha_config.fg_pixel_alpha_en) {
-				req->alpha_config.fg_global_alpha_en = true;
-				req->alpha_config.fg_global_alpha_value = 0xff;
-			}
+			if (req_rga->feature.global_alpha_en) {
+				if (req_rga->fg_global_alpha < 0xff) {
+					req->alpha_config.fg_global_alpha_en = true;
+					req->alpha_config.fg_global_alpha_value =
+						req_rga->fg_global_alpha;
+				} else if (!req->alpha_config.fg_pixel_alpha_en) {
+					req->alpha_config.fg_global_alpha_en = true;
+					req->alpha_config.fg_global_alpha_value = 0xff;
+				}
 
-			if (req_rga->bg_global_alpha < 0xff) {
-				req->alpha_config.bg_global_alpha_en = true;
-				req->alpha_config.bg_global_alpha_value = req_rga->bg_global_alpha;
-			} else if (!req->alpha_config.bg_pixel_alpha_en) {
-				req->alpha_config.bg_global_alpha_en = true;
+				if (req_rga->bg_global_alpha < 0xff) {
+					req->alpha_config.bg_global_alpha_en = true;
+					req->alpha_config.bg_global_alpha_value =
+						req_rga->bg_global_alpha;
+				} else if (!req->alpha_config.bg_pixel_alpha_en) {
+					req->alpha_config.bg_global_alpha_en = true;
+					req->alpha_config.bg_global_alpha_value = 0xff;
+				}
+			} else {
+				req->alpha_config.bg_global_alpha_value = 0xff;
 				req->alpha_config.bg_global_alpha_value = 0xff;
 			}
 

drivers/video/rockchip/rga3/include/rga.h
@@ -678,7 +678,11 @@ struct rga_req {
 	uint8_t fg_global_alpha;
 	uint8_t bg_global_alpha;
 
-	uint8_t reservr[57];
+	struct {
+		uint32_t global_alpha_en:1;
+	} feature;
+
+	uint8_t reservr[51];
 };
 
 struct rga_alpha_config {

armbian/linux-rockchip

commit bb21e49e98819a612b8c8eb1b43ec4452c750e1a

video: rockchip: rga3: fix typo in legacy global alpha config
which broke compatibility with librga API 1.9.x and older when using
dst-over mode.

Fixes: 7291693b01c8 ("video: rockchip: rga3: compatible with legacy global alpha config")

Change-Id: Ib73186b4617a5b27468e94d3394d5502206ad275
Signed-off-by: Yu Qiaowei <cerf.yu@rock-chips.com>

drivers/video/rockchip/rga3/rga2_reg_info.c
@@ -2637,7 +2637,7 @@ static void rga_cmd_to_rga2_cmd(struct rga_scheduler_t *scheduler,
 					req->alpha_config.bg_global_alpha_value = 0xff;
 				}
 			} else {
-				req->alpha_config.bg_global_alpha_value = 0xff;
+				req->alpha_config.fg_global_alpha_value = 0xff;
 				req->alpha_config.bg_global_alpha_value = 0xff;
 			}
 

drivers/video/rockchip/rga3/rga3_reg_info.c
@@ -1654,7 +1654,7 @@ static void rga_cmd_to_rga3_cmd(struct rga_req *req_rga, struct rga3_req *req)
 					req->alpha_config.bg_global_alpha_value = 0xff;
 				}
 			} else {
-				req->alpha_config.bg_global_alpha_value = 0xff;
+				req->alpha_config.fg_global_alpha_value = 0xff;
 				req->alpha_config.bg_global_alpha_value = 0xff;
 			}
 