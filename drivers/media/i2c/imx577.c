545f4f3c992c1f8c55b23f049715bffb7e9706ed
Yu Zheng <yu.zheng@rock-chips.com>
2025-12-01 18:03:56 +0800

    media: i2c: imx577: add otp support
    Signed-off-by: Yu Zheng <yu.zheng@rock-chips.com>
    Change-Id: I4843d9512d50d046cffb9b57d9a566ea0399b8ca

--- drivers/media/i2c/imx577.c
+++ drivers/media/i2c/imx577.c
@@ -38,6 +38,7 @@
 #include <linux/of_graph.h>
 #include <linux/pinctrl/consumer.h>
 #include <linux/rk-preisp.h>
+#include "otp_eeprom.h"
 
 #define DRIVER_VERSION			KERNEL_VERSION(0, 0x01, 0x05)
 
@@ -218,6 +219,7 @@ struct imx577 {
 	struct v4l2_fwnode_endpoint bus_cfg;
 	struct rkmodule_awb_cfg	awb_cfg;
 	struct rkmodule_lsc_cfg	lsc_cfg;
+	struct otp_info		*otp;
 };
 
 #define to_imx577(sd) container_of(sd, struct imx577, subdev)
@@ -1273,14 +1275,97 @@ static int imx577_g_frame_interval(struct v4l2_subdev *sd,
 	return 0;
 }
 
+static void imx577_get_otp(struct otp_info *otp,
+			    struct rkmodule_inf *inf)
+{
+	u32 i, j;
+	u32 w, h;
+
+	/* awb */
+	if (otp->awb_data.flag) {
+		inf->awb.flag = 1;
+		inf->awb.r_value = otp->awb_data.r_ratio;
+		inf->awb.b_value = otp->awb_data.b_ratio;
+		inf->awb.gr_value = otp->awb_data.g_ratio;
+		inf->awb.gb_value = 0x0;
+
+		inf->awb.golden_r_value = otp->awb_data.r_golden;
+		inf->awb.golden_b_value = otp->awb_data.b_golden;
+		inf->awb.golden_gr_value = otp->awb_data.g_golden;
+		inf->awb.golden_gb_value = 0x0;
+	}
+
+	/* lsc */
+	if (otp->lsc_data.flag) {
+		inf->lsc.flag = 1;
+		inf->lsc.width = otp->basic_data.size.width;
+		inf->lsc.height = otp->basic_data.size.height;
+		inf->lsc.table_size = otp->lsc_data.table_size;
+
+		for (i = 0; i < 289; i++) {
+			inf->lsc.lsc_r[i] = (otp->lsc_data.data[i * 2] << 8) |
+					     otp->lsc_data.data[i * 2 + 1];
+			inf->lsc.lsc_gr[i] = (otp->lsc_data.data[i * 2 + 578] << 8) |
+					      otp->lsc_data.data[i * 2 + 579];
+			inf->lsc.lsc_gb[i] = (otp->lsc_data.data[i * 2 + 1156] << 8) |
+					      otp->lsc_data.data[i * 2 + 1157];
+			inf->lsc.lsc_b[i] = (otp->lsc_data.data[i * 2 + 1734] << 8) |
+					     otp->lsc_data.data[i * 2 + 1735];
+		}
+	}
+
+	/* pdaf */
+	if (otp->pdaf_data.flag) {
+		inf->pdaf.flag = 1;
+		inf->pdaf.gainmap_width = otp->pdaf_data.gainmap_width;
+		inf->pdaf.gainmap_height = otp->pdaf_data.gainmap_height;
+		inf->pdaf.pd_offset = otp->pdaf_data.pd_offset;
+		inf->pdaf.dcc_mode = otp->pdaf_data.dcc_mode;
+		inf->pdaf.dcc_dir = otp->pdaf_data.dcc_dir;
+		inf->pdaf.dccmap_width = otp->pdaf_data.dccmap_width;
+		inf->pdaf.dccmap_height = otp->pdaf_data.dccmap_height;
+		w = otp->pdaf_data.gainmap_width;
+		h = otp->pdaf_data.gainmap_height;
+		for (i = 0; i < h; i++) {
+			for (j = 0; j < w; j++) {
+				inf->pdaf.gainmap[i * w + j] =
+					(otp->pdaf_data.gainmap[(i * w + j) * 2] << 8) |
+					otp->pdaf_data.gainmap[(i * w + j) * 2 + 1];
+			}
+		}
+		w = otp->pdaf_data.dccmap_width;
+		h = otp->pdaf_data.dccmap_height;
+		for (i = 0; i < h; i++) {
+			for (j = 0; j < w; j++) {
+				inf->pdaf.dccmap[i * w + j] =
+					(otp->pdaf_data.dccmap[(i * w + j) * 2] << 8) |
+					otp->pdaf_data.dccmap[(i * w + j) * 2 + 1];
+			}
+		}
+	}
+
+	/* af */
+	if (otp->af_data.flag) {
+		inf->af.flag = 1;
+		inf->af.dir_cnt = 1;
+		inf->af.af_otp[0].vcm_start = otp->af_data.af_inf;
+		inf->af.af_otp[0].vcm_end = otp->af_data.af_macro;
+		inf->af.af_otp[0].vcm_dir = 0;
+	}
+}
+
+
 static void imx577_get_module_inf(struct imx577 *imx577,
 				   struct rkmodule_inf *inf)
 {
+	struct otp_info *otp = imx577->otp;
 	memset(inf, 0, sizeof(*inf));
 	strscpy(inf->base.sensor, IMX577_NAME, sizeof(inf->base.sensor));
 	strscpy(inf->base.module, imx577->module_name,
 		sizeof(inf->base.module));
 	strscpy(inf->base.lens, imx577->len_name, sizeof(inf->base.lens));
+	if (otp)
+		imx577_get_otp(otp, inf);
 }
 
 static void imx577_set_awb_cfg(struct imx577 *imx577,
@@ -2260,6 +2345,11 @@ static int imx577_probe(struct i2c_client *client,
 	struct v4l2_subdev *sd;
 	struct device_node *endpoint;
 	char facing[2];
+
+	struct i2c_client *eeprom_ctrl_client;
+	struct device_node *eeprom_ctrl_node;
+	struct v4l2_subdev *eeprom_ctrl;
+	struct otp_info *otp_ptr;
 	int ret;
 	u32 i, hdr_mode = 0;
 
@@ -2365,6 +2455,38 @@ static int imx577_probe(struct i2c_client *client,
 	ret = imx577_check_sensor_id(imx577, client);
 	if (ret)
 		goto err_power_off;
+
+	eeprom_ctrl_node = of_parse_phandle(node, "eeprom-ctrl", 0);
+	if (eeprom_ctrl_node) {
+		eeprom_ctrl_client =
+			of_find_i2c_device_by_node(eeprom_ctrl_node);
+		of_node_put(eeprom_ctrl_node);
+		if (IS_ERR_OR_NULL(eeprom_ctrl_client)) {
+			dev_err(dev, "can not get node\n");
+			ret = -EINVAL;
+			goto err_free_handler;
+		}
+		eeprom_ctrl = i2c_get_clientdata(eeprom_ctrl_client);
+		if (IS_ERR_OR_NULL(eeprom_ctrl)) {
+			dev_err(dev, "can not get eeprom i2c client\n");
+		} else {
+			otp_ptr = devm_kzalloc(dev, sizeof(*otp_ptr), GFP_KERNEL);
+			if (!otp_ptr) {
+				put_device(&eeprom_ctrl_client->dev);
+				ret = -ENOMEM;
+				goto err_free_handler;
+			}
+			ret = v4l2_subdev_call(eeprom_ctrl,
+				core, ioctl, 0, otp_ptr);
+			if (!ret) {
+				imx577->otp = otp_ptr;
+			} else {
+				imx577->otp = NULL;
+				devm_kfree(dev, otp_ptr);
+			}
+		}
+		put_device(&eeprom_ctrl_client->dev);
+	}
 
 #ifdef CONFIG_VIDEO_V4L2_SUBDEV_API
 	sd->internal_ops = &imx577_internal_ops;
