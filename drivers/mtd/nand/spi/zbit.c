9a1f91664007b730728deace931acd281a8716ec
Jon Lin <jon.lin@rock-chips.com>
2025-12-05 20:21:15 +0800

    mtd: spinand: Zbit: Support new device ZB35Q0XCYIG
    ZB35Q01CYIG, ZB35Q02CYIG and ZB35Q04CYIG.
    
    Change-Id: Iaf2e0e9bcd10342fe860a1a97c1b98bc31a13c58
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/zbit.c
+++ drivers/mtd/nand/spi/zbit.c
@@ -76,6 +76,36 @@ static int zb35q04byig_ecc_get_status(struct spinand_device *spinand,
 	return -EINVAL;
 }
 
+static int zb35q0xc_ooblayout_ecc(struct mtd_info *mtd, int section,
+				   struct mtd_oob_region *region)
+{
+	if (section)
+		return -ERANGE;
+
+	region->offset = mtd->oobsize / 2;
+	region->length = mtd->oobsize / 2;
+
+	return 0;
+}
+
+static int zb35q0xc_ooblayout_free(struct mtd_info *mtd, int section,
+				    struct mtd_oob_region *region)
+{
+	if (section)
+		return -ERANGE;
+
+	/* Reserve 2 bytes for the BBM. */
+	region->offset = 2;
+	region->length = mtd->oobsize / 2 - 2;
+
+	return 0;
+}
+
+static const struct mtd_ooblayout_ops zb35q0xc_ooblayout = {
+	.ecc = zb35q0xc_ooblayout_ecc,
+	.free = zb35q0xc_ooblayout_free,
+};
+
 static const struct spinand_info zbit_spinand_table[] = {
 	SPINAND_INFO("ZB35Q01BYIG",
 		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0xA1),
@@ -88,6 +118,33 @@ static const struct spinand_info zbit_spinand_table[] = {
 		     SPINAND_ECCINFO(&zb35q01b_ooblayout, NULL)),
 	SPINAND_INFO("ZB35Q04BYIG",
 		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0xA3),
+		     NAND_MEMORG(1, 2048, 128, 128, 2048, 40, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&zb35q01b_ooblayout, zb35q04byig_ecc_get_status)),
+	SPINAND_INFO("ZB35Q01CYIG",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0xC1),
+		     NAND_MEMORG(1, 2048, 64, 64, 1024, 20, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&zb35q0xc_ooblayout, zb35q04byig_ecc_get_status)),
+	SPINAND_INFO("ZB35Q02CYIG",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0xC2),
+		     NAND_MEMORG(1, 2048, 64, 64, 2048, 40, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&zb35q0xc_ooblayout, zb35q04byig_ecc_get_status)),
+	SPINAND_INFO("ZB35Q04CYIG",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0xC3),
 		     NAND_MEMORG(1, 2048, 128, 128, 2048, 40, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
