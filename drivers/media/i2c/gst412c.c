8dbf3c3edc7a809970b160b888e6dcf6c58c0dd1
Tao Huang <huangtao@rock-chips.com>
2025-12-12 14:27:42 +0800

    media: i2c: gst412c: Drop the unused id parameter from gst412c_probe()
    Change-Id: Ib038e176688073b7ac5c52a31c915d1088bff99f
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/media/i2c/gst412c.c
+++ drivers/media/i2c/gst412c.c
@@ -1142,8 +1142,7 @@ static int gst412c_check_sensor_id(struct gst412c *gst412c,
 	return 0;
 }
 
-static int gst412c_probe(struct i2c_client *client,
-			 const struct i2c_device_id *id)
+static int gst412c_probe(struct i2c_client *client)
 {
 	struct device *dev = &client->dev;
 	struct device_node *node = dev->of_node;
