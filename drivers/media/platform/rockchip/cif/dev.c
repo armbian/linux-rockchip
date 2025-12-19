067f21ad0a3a034642a19f7b9516aca0e100629e
Zefa Chen <zefa.chen@rock-chips.com>
2025-12-19 11:41:04 +0800

    media: rockchip: vicap one to multi support single exposure mode
    Change-Id: Idf5ab03e3816bf88ee4b99c3b4445f0f5670af4e
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>

--- drivers/media/platform/rockchip/cif/dev.c
+++ drivers/media/platform/rockchip/cif/dev.c
@@ -2816,7 +2816,12 @@ static void rkcif_exp_work(struct work_struct *exp_work)
 		sditf_event_inc_sof(priv);
 
 	if (stream->frame_idx == 0) {
-		cur_time = priv->cur_time;
+		if (dev->channels[0].capture_info.one_to_multi.exp_mode == RKMODULE_ONE_TO_MULT_EXP_SINGLE &&
+		    dev->sditf[dev->channels[0].capture_info.one_to_multi.exp_main_id]) {
+			cur_time = dev->sditf[dev->channels[0].capture_info.one_to_multi.exp_main_id]->cur_time;
+		} else {
+			cur_time = priv->cur_time;
+		}
 	} else {
 		effect_frame = stream->frame_idx + dev->exp_delay.time_delay - 1;
 		id = rkcif_get_exp_effect_stream_id(dev, effect_frame);
@@ -2838,7 +2843,12 @@ static void rkcif_exp_work(struct work_struct *exp_work)
 				kfree(time);
 			}
 		} else {
-			cur_time = priv->cur_time;
+			if (dev->channels[0].capture_info.one_to_multi.exp_mode == RKMODULE_ONE_TO_MULT_EXP_SINGLE &&
+			    dev->sditf[dev->channels[0].capture_info.one_to_multi.exp_main_id]) {
+				cur_time = dev->sditf[dev->channels[0].capture_info.one_to_multi.exp_main_id]->cur_time;
+			} else {
+				cur_time = priv->cur_time;
+			}
 		}
 		if (dev->exp_dbg)
 			dev_info(priv->dev, "exp set id %d, val 0x%x\n",
@@ -2849,7 +2859,12 @@ static void rkcif_exp_work(struct work_struct *exp_work)
 	v4l2_ctrl_s_ctrl(ctrl, cur_time);
 	priv->cur_time = cur_time;
 	if (stream->frame_idx == 0) {
-		cur_gain = priv->cur_gain;
+		if (dev->channels[0].capture_info.one_to_multi.exp_mode == RKMODULE_ONE_TO_MULT_EXP_SINGLE &&
+		    dev->sditf[dev->channels[0].capture_info.one_to_multi.exp_main_id]) {
+			cur_gain = dev->sditf[dev->channels[0].capture_info.one_to_multi.exp_main_id]->cur_gain;
+		} else {
+			cur_gain = priv->cur_gain;
+		}
 	} else {
 		effect_frame = stream->frame_idx + dev->exp_delay.gain_delay - 1;
 		id = rkcif_get_exp_effect_stream_id(dev, effect_frame);
@@ -2871,7 +2886,12 @@ static void rkcif_exp_work(struct work_struct *exp_work)
 				kfree(gain);
 			}
 		} else {
-			cur_gain = priv->cur_gain;
+			if (dev->channels[0].capture_info.one_to_multi.exp_mode == RKMODULE_ONE_TO_MULT_EXP_SINGLE &&
+			    dev->sditf[dev->channels[0].capture_info.one_to_multi.exp_main_id]) {
+				cur_gain = dev->sditf[dev->channels[0].capture_info.one_to_multi.exp_main_id]->cur_gain;
+			} else {
+				cur_gain = priv->cur_gain;
+			}
 		}
 		if (dev->exp_dbg)
 			dev_info(priv->dev, "gain set id %d, val 0x%x\n",
