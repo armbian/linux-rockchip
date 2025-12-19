5c7da552bdc625b544e0632e76aae158b037abd0
Tao Huang <huangtao@rock-chips.com>
2025-12-19 19:49:08 +0800

    MALI: midgard: Use generic MIN() and MAX() macros
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: I93c63a96ebb9fc63b7b542c76f9c00ba4b399a52

--- drivers/gpu/arm/midgard/mali_malisw.h
+++ drivers/gpu/arm/midgard/mali_malisw.h
@@ -22,6 +22,7 @@
 #ifndef _MALISW_H_
 #define _MALISW_H_
 
+#include <linux/minmax.h>
 #include <linux/version.h>
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 14, 0)
 #define U8_MAX          ((u8)~0U)
@@ -47,7 +48,9 @@
  * As a macro it may evaluate its arguments more than once.
  * Refer to MAX macro for more details
  */
+#ifndef MIN
 #define MIN(x, y)	((x) < (y) ? (x) : (y))
+#endif
 
 /**
  * MAX -  Return the greater of two values.
@@ -59,7 +62,9 @@
  * to retrieve the min and max of two values, consider using a conditional swap
  * instead.
  */
+#ifndef MAX
 #define MAX(x, y)	((x) < (y) ? (y) : (x))
+#endif
 
 /**
  * @hideinitializer
