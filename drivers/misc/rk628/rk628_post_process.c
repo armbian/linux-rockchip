d07b99f1b15743903f9a13ea3be269891ddefdb7
Tao Huang <huangtao@rock-chips.com>
2025-12-19 19:46:31 +0800

    misc: rk628: Use generic MIN() and MAX() macros
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>
    Change-Id: Ie008aa967f64b0ff28cf3b4a4c74da0aaecdc27c

--- drivers/misc/rk628/rk628_post_process.c
+++ drivers/misc/rk628/rk628_post_process.c
@@ -44,8 +44,12 @@
 /* r,g,b color temp div coef, range [-128,128] for 10bit data */
 #define PQ_CSC_TEMP_OFFSET_DIV_COEF		2
 
+#ifndef MAX
 #define	MAX(a, b)				((a) > (b) ? (a) : (b))
+#endif
+#ifndef MIN
 #define	MIN(a, b)				((a) < (b) ? (a) : (b))
+#endif
 #define	CLIP(x, min_v, max_v)			MIN(MAX(x, min_v), max_v)
 
 #define V4L2_COLORSPACE_BT709F	0xfe
