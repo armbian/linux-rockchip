2794bd38f0e932093dea76945574b3f129c63288
Jon Lin <jon.lin@rock-chips.com>
2026-01-06 16:48:53 +0800

    mtd: spinand: Winbond: Fix W25N08LWZEIG oob layout
    Change-Id: I8c2068349d6ea3cc7c9cc03229e9c8b91243dc7f
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/winbond.c
+++ drivers/mtd/nand/spi/winbond.c
@@ -173,6 +173,35 @@ static const struct mtd_ooblayout_ops w25n04lw_ooblayout = {
 	.free = w25n04lw_ooblayout_free,
 };
 
+static int w25n08lw_ooblayout_ecc(struct mtd_info *mtd, int section,
+				  struct mtd_oob_region *region)
+{
+	if (section > 7)
+		return -ERANGE;
+
+	region->offset = 128 + (16 * section);
+	region->length = 13;
+
+	return 0;
+}
+
+static int w25n08lw_ooblayout_free(struct mtd_info *mtd, int section,
+				   struct mtd_oob_region *region)
+{
+	if (section > 7)
+		return -ERANGE;
+
+	region->offset = (16 * section) + 2;
+	region->length = 14;
+
+	return 0;
+}
+
+static const struct mtd_ooblayout_ops w25n08lw_ooblayout = {
+	.ecc = w25n08lw_ooblayout_ecc,
+	.free = w25n08lw_ooblayout_free,
+};
+
 static const struct spinand_info winbond_spinand_table[] = {
 	/* 512M-bit densities */
 	SPINAND_INFO("W25N512GV",
@@ -297,7 +326,7 @@ static const struct spinand_info winbond_spinand_table[] = {
 		     0,
 		     SPINAND_ECCINFO(&w25n04lw_ooblayout, w25n02kv_ecc_get_status)),
 	/* 8G-bit densities */
-	SPINAND_INFO("W25N08LW2EIG",
+	SPINAND_INFO("W25N08LWZEIG",
 		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0xB3, 0x24),
 		     NAND_MEMORG(1, 4096, 128, 64, 4096, 40, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
@@ -305,7 +334,7 @@ static const struct spinand_info winbond_spinand_table[] = {
 					      &write_cache_variants,
 					      &update_cache_variants),
 		     0,
-		     SPINAND_ECCINFO(&w25n02kv_ooblayout, w25n02kv_ecc_get_status)),
+		     SPINAND_ECCINFO(&w25n08lw_ooblayout, w25n02kv_ecc_get_status)),
 };
 
 static int winbond_spinand_init(struct spinand_device *spinand)
