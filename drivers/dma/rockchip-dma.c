khusika/linux_stable-orangepi

commit 081aacf209eaf1c6061f0db4c1f936ec3770a2e3

dmaengine: rockchip-dma: Call rk_pdma_driver_init() early when CONFIG_ROCKCHIP_THUNDER_BOOT=y
Moving rk_pdma_driver_init() to the arch_initcall_sync level.
Before rk3x_i2c_driver_init() etc.

Change-Id: Iafcb84b1537453ffb295bb7722d4455e75657e9c
Signed-off-by: Tao Huang <huangtao@rock-chips.com>

drivers/dma/rockchip-dma.c
@@ -1353,7 +1353,21 @@ static struct platform_driver rk_pdma_driver = {
 	.remove		= rk_dma_remove,
 };
 
+#ifdef CONFIG_ROCKCHIP_THUNDER_BOOT
+static int __init rk_pdma_driver_init(void)
+{
+	return platform_driver_register(&rk_pdma_driver);
+}
+arch_initcall_sync(rk_pdma_driver_init);
+
+static void __exit rk_pdma_driver_exit(void)
+{
+	platform_driver_unregister(&rk_pdma_driver);
+}
+module_exit(rk_pdma_driver_exit);
+#else
 module_platform_driver(rk_pdma_driver);
+#endif
 
 MODULE_DESCRIPTION("Rockchip DMA Driver");
 MODULE_AUTHOR("Sugar.Zhang@rock-chips.com");

armbian/linux-rockchip

commit 33544b5c01a7dda2a83b7c45df2711e6b3a77141

dmaengine: rockchip-dma: Fix module compilation when CONFIG_ROCKCHIP_THUNDER_BOOT=y
drivers/dma/rockchip-dma.c:1516:1: error: data definition has no type or storage class [-Werror]
 1516 | arch_initcall_sync(rk_pdma_driver_init);
      | ^~~~~~~~~~~~~~~~~~
drivers/dma/rockchip-dma.c:1516:1: error: type defaults to 'int' in declaration of 'arch_initcall_sync' [-Werror=implicit-int]

Fixes: 081aacf209ea ("dmaengine: rockchip-dma: Call rk_pdma_driver_init() early when CONFIG_ROCKCHIP_THUNDER_BOOT=y")
Change-Id: I5971fdcce7cafeae4b0990a1561edd77c19375bf
Signed-off-by: Tao Huang <huangtao@rock-chips.com>

drivers/dma/rockchip-dma.c
@@ -1508,7 +1508,7 @@ static struct platform_driver rk_pdma_driver = {
 	.remove		= rk_dma_remove,
 };
 
-#ifdef CONFIG_ROCKCHIP_THUNDER_BOOT
+#if !defined(MODULE) && defined(CONFIG_ROCKCHIP_THUNDER_BOOT)
 static int __init rk_pdma_driver_init(void)
 {
 	return platform_driver_register(&rk_pdma_driver);