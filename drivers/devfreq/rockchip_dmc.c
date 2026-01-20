4cb03438e55c146365949ce8799fe535750e4fe5
YouMin Chen <cym@rock-chips.com>
2026-01-20 19:56:08 +0800

    PM / devfreq: rockchip_dmc: Add support for rk3572
    Change-Id: I6dc16c2da778a15a0c2e62a195e3cf9111e07809
    Signed-off-by: YouMin Chen <cym@rock-chips.com>

--- drivers/devfreq/rockchip_dmc.c
+++ drivers/devfreq/rockchip_dmc.c
@@ -2146,6 +2146,9 @@ static const struct of_device_id rockchip_dmcfreq_of_match[] = {
 #if IS_ENABLED(CONFIG_CPU_RK3568)
 	{ .compatible = "rockchip,rk3568-dmc", .data = rk3568_dmc_init },
 #endif
+#if IS_ENABLED(CONFIG_CPU_RK3572)
+	{ .compatible = "rockchip,rk3572-dmc", .data = rk3588_dmc_init },
+#endif
 #if IS_ENABLED(CONFIG_CPU_RK3576)
 	{ .compatible = "rockchip,rk3576-dmc", .data = rk3588_dmc_init },
 #endif
