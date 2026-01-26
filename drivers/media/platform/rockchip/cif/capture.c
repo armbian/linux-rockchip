4619c7d4df0b404b7aeb43a5fbdc47b7e6ef032c
Zefa Chen <zefa.chen@rock-chips.com>
2026-01-26 10:08:30 +0800

    media: rockchip: vicap fixes error of buffer rotate after reset work with soft interlace
    Change-Id: Ief3dd72c18f28847346adf22352d624f440aab68
    Signed-off-by: Zefa Chen <zefa.chen@rock-chips.com>

--- drivers/media/platform/rockchip/cif/capture.c
+++ drivers/media/platform/rockchip/cif/capture.c
@@ -15502,8 +15502,10 @@ static void rkcif_check_mipi_interlaced_frame_id(struct rkcif_stream *stream)
 		 stream->id, fs_interlaced_phase, frame_id);
 	if (stream->last_fs_interlaced_phase == 0) {
 		buffer = stream->curr_buf;
-		rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME0_READY, buf_offset);
-		rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME1_READY, buf_offset);
+		if (buffer) {
+			rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME0_READY, buf_offset);
+			rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME1_READY, buf_offset);
+		}
 	} else {
 		if (fs_interlaced_phase == CIF_CSI_FRAME0_READY) {
 			buffer = stream->curr_buf;
@@ -15519,12 +15521,16 @@ static void rkcif_check_mipi_interlaced_frame_id(struct rkcif_stream *stream)
 						 "stream[%d] mipi fs interlace bad frame queue, phase %d, line %d\n",
 						 stream->id, fs_interlaced_phase, __LINE__);
 				}
-				rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME0_READY, buf_offset);
-				rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME1_READY, buf_offset);
+				if (buffer) {
+					rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME0_READY, buf_offset);
+					rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME1_READY, buf_offset);
+				}
 			} else if (stream->last_fs_interlaced_phase == CIF_CSI_FRAME0_READY) {
 				stream->interlaced_bad_frame = true;
-				rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME0_READY, buf_offset);
-				rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME1_READY, buf_offset);
+				if (buffer) {
+					rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME0_READY, buf_offset);
+					rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME1_READY, buf_offset);
+				}
 				v4l2_dbg(rkcif_debug, 3, &dev->v4l2_dev,
 					 "stream[%d] mipi fs interlace bad frame, phase %d, line %d\n",
 					 stream->id, fs_interlaced_phase, __LINE__);
@@ -15532,11 +15538,10 @@ static void rkcif_check_mipi_interlaced_frame_id(struct rkcif_stream *stream)
 		} else {
 			if (stream->last_fs_interlaced_phase == CIF_CSI_FRAME0_READY) {
 				if (stream->next_buf) {
-					if (stream->curr_buf) {
+					if (stream->curr_buf)
 						rkcif_buf_queue(&stream->curr_buf->vb.vb2_buf);
-						stream->curr_buf = stream->next_buf;
-						stream->next_buf = NULL;
-					}
+					stream->curr_buf = stream->next_buf;
+					stream->next_buf = NULL;
 					stream->interlaced_bad_frame = true;
 					v4l2_dbg(rkcif_debug, 3, &dev->v4l2_dev,
 						 "stream[%d] mipi fs interlace bad frame, phase %d\n",
@@ -15554,7 +15559,6 @@ static void rkcif_check_mipi_interlaced_frame_id(struct rkcif_stream *stream)
 					rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME0_READY, buf_offset);
 					rkcif_write_buffer(stream, buffer, CIF_CSI_FRAME1_READY, buf_offset);
 				} else if (dummy_buf->is_allocated) {
-					stream->interlaced_bad_frame = true;
 					rkcif_write_buffer(stream, NULL, CIF_CSI_FRAME0_READY, buf_offset);
 					rkcif_write_buffer(stream, NULL, CIF_CSI_FRAME1_READY, buf_offset);
 					v4l2_dbg(rkcif_debug, 3, &dev->v4l2_dev,
@@ -15588,7 +15592,6 @@ static void rkcif_check_mipi_interlaced_frame_id(struct rkcif_stream *stream)
 				} else if (dummy_buf->is_allocated) {
 					rkcif_write_buffer(stream, NULL, CIF_CSI_FRAME0_READY, buf_offset);
 					rkcif_write_buffer(stream, NULL, CIF_CSI_FRAME1_READY, buf_offset);
-					stream->interlaced_bad_frame = true;
 					v4l2_dbg(rkcif_debug, 3, &dev->v4l2_dev,
 						 "stream[%d] mipi interlace not buf, use dummy buf, line %d\n",
 						 stream->id, __LINE__);
