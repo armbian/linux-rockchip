022a4fe843f3f5895c7ce1db27fe55b60acebf86
Damon Ding <damon.ding@rock-chips.com>
2026-01-27 22:22:55 +0800

    phy: rockchip: samsung-hdptx: Add platform data for RK3572
    Change-Id: I04ad6efecc352d53b852f0a19536350e6eb4f024
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/phy/rockchip/phy-rockchip-samsung-hdptx.c
+++ drivers/phy/rockchip/phy-rockchip-samsung-hdptx.c
@@ -2001,6 +2001,13 @@ static const struct dev_pm_ops rk_hdptx_phy_pm_ops = {
 		       rk_hdptx_phy_runtime_resume, NULL)
 };
 
+static const struct rk_hdptx_phy_cfg rk3572_hdptx_phy_cfgs = {
+	.num_phys = 1,
+	.phy_ids = {
+		0x2c310000,
+	},
+};
+
 static const struct rk_hdptx_phy_cfg rk3576_hdptx_phy_cfgs = {
 	.num_phys = 1,
 	.phy_ids = {
@@ -2017,6 +2024,10 @@ static const struct rk_hdptx_phy_cfg rk3588_hdptx_phy_cfgs = {
 };
 
 static const struct of_device_id rk_hdptx_phy_of_match[] = {
+	{
+		.compatible = "rockchip,rk3572-hdptx-phy",
+		.data = &rk3572_hdptx_phy_cfgs
+	},
 	{
 		.compatible = "rockchip,rk3576-hdptx-phy",
 		.data = &rk3576_hdptx_phy_cfgs
