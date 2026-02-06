90d193dabe4691b62277d77d83a5fe749f547a86
Jon Lin <jon.lin@rock-chips.com>
2026-02-06 09:40:03 +0800

    misc: rockchip: pcie-rkep: fix memory leak in open/release path
    The pcie_rkep_open function incorrectly used devm_kzalloc() to allocate
    memory for struct pcie_file. Since devm_ resources are bound to the
    lifetime of the PCI device and not the file descriptor, repeated
    open/close operations on the misc device caused memory to accumulate
    without being freed, leading to a significant memory leak.
    
    Fix this by switching to kzalloc() for manual memory management.
    Correspondingly, add mutex_destroy() and kfree() in pcie_rkep_release()
    to properly clean up the resources when the device file is closed.
    
    Change-Id: I6175e10aec1ec6dc9ff00684c7572b16448920bb
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/misc/rockchip/pcie-rkep.c
+++ drivers/misc/rockchip/pcie-rkep.c
@@ -455,7 +455,7 @@ static int pcie_rkep_open(struct inode *inode, struct file *file)
 	struct pcie_rkep *pcie_rkep = container_of(miscdev, struct pcie_rkep, dev);
 	struct pcie_file *pcie_file = NULL;
 
-	pcie_file = devm_kzalloc(&pcie_rkep->pdev->dev, sizeof(struct pcie_file), GFP_KERNEL);
+	pcie_file = kzalloc(sizeof(struct pcie_file), GFP_KERNEL);
 	if (!pcie_file)
 		return -ENOMEM;
 
@@ -503,6 +503,9 @@ static int pcie_rkep_release(struct inode *inode, struct file *file)
 		kfree(buffer_req);
 	}
 	mutex_unlock(&pcie_file->file_lock_mutex);
+
+	kfree(pcie_file);
+	file->private_data = NULL;
 
 	return 0;
 }
