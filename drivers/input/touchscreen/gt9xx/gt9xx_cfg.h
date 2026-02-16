999ccd0969be07fe26aba2f4975a6f4a2ee0c927
Florian Kohn <florian.kohn@gmail.com>
2026-02-16 15:39:39 +0100

    Added Radxa display 10fhd for rock 5b plus

--- drivers/input/touchscreen/gt9xx/gt9xx_cfg.h
+++ drivers/input/touchscreen/gt9xx/gt9xx_cfg.h
@@ -33,6 +33,11 @@ static u8 gtp_dat_gt9111[] = {
 	#include "HLS-0102-1398V1-1060-GT911_Config_20201204_V66.cfg"
 };
 
+static u8 gtp_dat_gt9272[] = {
+	/* <1200, 1920> */
+	#include "CJ080258_GT9271_Config_20230608_112016.cfg"
+};
+
 static u8 gtp_dat_8_9[] = {
 	/* TODO:Puts your update firmware data here! */
 	/* <1920, 1200> 8.9 */
