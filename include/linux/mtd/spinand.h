b2d90f88e4e88a9795b1ec08a7cbc56fee52ad0d
Jon Lin <jon.lin@rock-chips.com>
2026-03-13 12:46:16 +0800

    mtd: spinand: MK: Support new device MKSV1GIL-AE and MKSV2GIL-AE
    Change-Id: I3267826e342390b6185760e3d4379fbcce16e00f
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- include/linux/mtd/spinand.h
+++ include/linux/mtd/spinand.h
@@ -278,6 +278,7 @@ extern const struct spinand_manufacturer jsc_spinand_manufacturer;
 extern const struct spinand_manufacturer kingston_spinand_manufacturer;
 extern const struct spinand_manufacturer macronix_spinand_manufacturer;
 extern const struct spinand_manufacturer micron_spinand_manufacturer;
+extern const struct spinand_manufacturer mk_spinand_manufacturer;
 extern const struct spinand_manufacturer paragon_spinand_manufacturer;
 extern const struct spinand_manufacturer toshiba_spinand_manufacturer;
 extern const struct spinand_manufacturer silicongo_spinand_manufacturer;
