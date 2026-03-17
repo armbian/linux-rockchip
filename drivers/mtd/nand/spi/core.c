70bcfa3eea9656d5176cd99ca4cafbf5eb94924b
Jon Lin <jon.lin@rock-chips.com>
2026-03-17 08:52:26 +0800

    mtd: spinand: CHUCUN: Support new device C5F1GM7RE
    Change-Id: Ifa09288626c872d9f473000d01e4d6e256ccc032
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -1160,6 +1160,9 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 #ifdef CONFIG_MTD_SPI_NAND_BIWIN
 	&biwin_spinand_manufacturer,
 #endif
+#ifdef CONFIG_MTD_SPI_NAND_CHUCUN
+	&chucun_spinand_manufacturer,
+#endif
 #ifdef CONFIG_MTD_SPI_NAND_DOSILICON
 	&dosilicon_spinand_manufacturer,
 #endif
--- include/linux/mtd/spinand.h
+++ include/linux/mtd/spinand.h
@@ -264,6 +264,7 @@ struct spinand_manufacturer {
 extern const struct spinand_manufacturer alliancememory_spinand_manufacturer;
 extern const struct spinand_manufacturer ato_spinand_manufacturer;
 extern const struct spinand_manufacturer biwin_spinand_manufacturer;
+extern const struct spinand_manufacturer chucun_spinand_manufacturer;
 extern const struct spinand_manufacturer dosilicon_spinand_manufacturer;
 extern const struct spinand_manufacturer esmt_8c_spinand_manufacturer;
 extern const struct spinand_manufacturer esmt_c8_spinand_manufacturer;
