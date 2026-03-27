aa97e96cb675f5f540803fe6ee573c2c3192a236
Frank Wang <frank.wang@rock-chips.com>
2026-03-27 15:12:03 +0800

    phy: rockchip: inno-usb2: fix node put defect
    Invoke of_node_put() and assign to NULL when breaking out from
    for_each_available_child_of_node() to avoid memory leak.
    
    Signed-off-by: Frank Wang <frank.wang@rock-chips.com>
    Change-Id: I88f1b3eb2d75ec292e3f258f50d626f265fff297

--- drivers/phy/rockchip/phy-rockchip-inno-usb2.c
+++ drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -2621,8 +2621,11 @@ static int rockchip_usb2phy_probe(struct platform_device *pdev)
 
 next_child:
 		/* to prevent out of boundary */
-		if (++index >= rphy->phy_cfg->num_ports)
+		if (++index >= rphy->phy_cfg->num_ports) {
+			of_node_put(child_np);
+			child_np = NULL;
 			break;
+		}
 	}
 
 	provider = devm_of_phy_provider_register(dev, of_phy_simple_xlate);
