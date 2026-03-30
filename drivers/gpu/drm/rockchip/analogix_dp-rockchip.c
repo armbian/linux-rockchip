43d73cbd0dfe25548e062da1889228e276f2946b
Damon Ding <damon.ding@rock-chips.com>
2026-03-30 21:14:06 +0800

    drm/rockchip: analogix_dp: Add &rockchip_dp_chip_data.max_dclk_khz for mode validation
    The max dclk rate varies across platforms. Add this variable to filter
    out modes with &drm_display_mode.clock exceeding the limit.
    
    Change-Id: I7118f477761079f97082077fa31371065bfbc8fe
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/gpu/drm/rockchip/analogix_dp-rockchip.c
+++ drivers/gpu/drm/rockchip/analogix_dp-rockchip.c
@@ -58,6 +58,7 @@ struct rockchip_grf_reg_field {
  * @edp_mode: grf register field of edp_mode
  * @mem_clk_auto_gating: grf register field of mem_clk_auto_gating
  * @chip_type: specific chip type
+ * @max_dclk_khz: the maximum supported dclk rate
  * @ssc: check if SSC is supported by source
  * @audio: check if audio is supported by source
  * @split_mode: check if split mode is supported
@@ -72,6 +73,7 @@ struct rockchip_dp_chip_data {
 	const struct rockchip_grf_reg_field edp_mode;
 	const struct rockchip_grf_reg_field mem_clk_auto_gating;
 	u32	chip_type;
+	u32	max_dclk_khz;
 	bool	ssc;
 	bool	audio;
 	bool	split_mode;
@@ -373,6 +375,12 @@ rockchip_dp_drm_encoder_mode_valid(struct drm_encoder *encoder,
 	if (!vm.hfront_porch || !vm.hback_porch || !vm.vfront_porch || !vm.vback_porch) {
 		DRM_DEV_ERROR(dp->dev, "front porch or back porch can not be 0\n");
 		return MODE_BAD;
+	}
+
+	if (mode->clock > dp->data->max_dclk_khz) {
+		dev_err(dp->dev, "clock[%dkHz] exceeds limit[%dkHz]\n",
+			mode->clock, dp->data->max_dclk_khz);
+		return MODE_CLOCK_HIGH;
 	}
 
 	return MODE_OK;
@@ -911,6 +919,7 @@ static const struct rockchip_dp_chip_data rk3399_edp[] = {
 		.chip_type = RK3399_EDP,
 		.lcdc_sel = GRF_REG_FIELD(0x6250, 5, 5),
 		.ssc = true,
+		.max_dclk_khz = 350000,
 	},
 	{ /* sentinel */ }
 };
@@ -920,6 +929,7 @@ static const struct rockchip_dp_chip_data rk3288_dp[] = {
 		.chip_type = RK3288_DP,
 		.lcdc_sel = GRF_REG_FIELD(0x025c, 5, 5),
 		.ssc = true,
+		.max_dclk_khz = 350000,
 	},
 	{ /* sentinel */ }
 };
@@ -929,6 +939,7 @@ static const struct rockchip_dp_chip_data rk3568_edp[] = {
 		.chip_type = RK3568_EDP,
 		.ssc = true,
 		.audio = true,
+		.max_dclk_khz = 350000,
 	},
 	{ /* sentinel */ }
 };
@@ -945,6 +956,7 @@ static const struct rockchip_dp_chip_data rk3576_edp[] = {
 		.format_yuv = true,
 		.support_dp_mode = true,
 		.max_bpc = 10,
+		.max_dclk_khz = 600000,
 	},
 	{ /* sentinel */ }
 };
@@ -961,6 +973,7 @@ static const struct rockchip_dp_chip_data rk3588_edp[] = {
 		.format_yuv = true,
 		.support_dp_mode = true,
 		.max_bpc = 10,
+		.max_dclk_khz = 600000,
 	},
 	{
 		.chip_type = RK3588_EDP,
@@ -973,6 +986,7 @@ static const struct rockchip_dp_chip_data rk3588_edp[] = {
 		.format_yuv = true,
 		.support_dp_mode = true,
 		.max_bpc = 10,
+		.max_dclk_khz = 600000,
 	},
 	{ /* sentinel */ }
 };
