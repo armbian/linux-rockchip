9a04707bd3e6f88b0ab0bc25f6bae4bf39d01d3f
Jon Lin <jon.lin@rock-chips.com>
2026-04-17 18:56:37 +0800

    mtd: spi-nor: zbit: Add support for ZB25Q256A
    Add support for Zbit ZB25Q256A 256Mbit SPI NOR flash.
    JEDEC ID: 0x5e4019
    Capacity: 256Mb (32MB)
    Features: 4KB sectors, Dual/Quad read, 4-byte opcodes support
    
    Change-Id: Ibf1e465a95c93f836f0d357fc3ffcc2e6e14b188
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/spi-nor/zbit.c
+++ drivers/mtd/spi-nor/zbit.c
@@ -17,6 +17,10 @@ static const struct flash_info zbit_parts[] = {
 	{ "ZB25LQ128", INFO(0x5e5018, 0, 64 * 1024, 256)
 		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ |
 			      SPI_NOR_QUAD_READ) },
+	{ "ZB25Q256A", INFO(0x5e4019, 0, 64 * 1024, 512)
+		NO_SFDP_FLAGS(SECT_4K | SPI_NOR_DUAL_READ |
+			      SPI_NOR_QUAD_READ)
+		FIXUP_FLAGS(SPI_NOR_4B_OPCODES) },
 };
 
 const struct spi_nor_manufacturer spi_nor_zbit = {
