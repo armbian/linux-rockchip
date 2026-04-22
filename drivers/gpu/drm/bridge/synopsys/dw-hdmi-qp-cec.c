armbian/linux-rockchip

commit c1851a5cf9fd4a0ef331ebe6f1062d367bb1f4eb

drm/bridge: dw-hdmi-qp-cec: Fix crash when cec is disabled
When cec is disabled(For example, disabling cec option in
android settings), the kernel will disable cec clock of hdmi
controller. At this time, if the cec register is accessed, it will
cause a crash:

[   58.125725][    C0] SError Interrupt on CPU0, code 0x00000000be000011
-- SError
[   58.125738][    C0] CPU: 0 PID: 512 Comm: binder:476_1 Tainted: G
O       6.1.141 #322
[   58.125743][    C0] Hardware name: Rockchip RK3538 EVB1 DDR4 V10
Board (DT)
[   58.125746][    C0] pstate: 20400009 (nzCv daif +PAN -UAO -TCO -DIT
-SSBS BTYPE=--)
[   58.125752][    C0] pc : _raw_spin_unlock_irqrestore+0x24/0x54
[   58.125767][    C0] lr : regmap_unlock_spinlock+0x14/0x20
[   58.125774][    C0] sp : ffffffc00a653b20
[   58.125775][    C0] x29: ffffffc00a653b20 x28: ffffff801a648000 x27:
0000000000000000
[   58.125784][    C0] x26: 0000000000000000 x25: 0000007fffffffa4 x24:
ffffff801a648000
[   58.125790][    C0] x23: ffffff8003df03e8 x22: 0000000000000000 x21:
0000000000004004
[   58.125795][    C0] x20: 0000000000000000 x19: ffffff8003679800 x18:
ffffffc00a5e5078
[   58.125801][    C0] x17: 00000000000158c5 x16: ffffffdcd17aae18 x15:
0000000000000000
[   58.125807][    C0] x14: 00000000000000e9 x13: 0000000000000005 x12:
000000000000000a
[   58.125812][    C0] x11: 0000000000000000 x10: ffffffdcd18eb488 x9 :
0000000100000001
[   58.125818][    C0] x8 : ffffff801a648000 x7 : 0000000000000000 x6 :
0000000000000000
[   58.125823][    C0] x5 : ffffffc00a653a70 x4 : 0000000000000001 x3 :
ffffffc00a653958
[   58.125828][    C0] x2 : 0000000000000000 x1 : 0000000000000000 x0 :
ffffff8003679800
[   58.125835][    C0] Kernel panic - not syncing: Asynchronous SError
Interrupt
[   58.125839][    C0] CPU: 0 PID: 512 Comm: binder:476_1 Tainted: G
O       6.1.141 #322
[   58.125843][    C0] Hardware name: Rockchip RK3538 EVB1 DDR4 V10
Board (DT)
[   58.125845][    C0] Call trace:
[   58.125846][    C0]  dump_backtrace+0xf4/0x114

drivers/gpu/drm/bridge/synopsys/dw-hdmi-qp-cec.c
@@ -96,6 +96,7 @@ struct dw_hdmi_qp_cec {
 	bool rx_done;
 	bool wake_en;
 	bool standby_en;
+	bool cec_enable;
 	struct cec_notifier *notify;
 	int irq;
 	int wake_irq;
@@ -139,6 +140,9 @@ static int dw_hdmi_qp_cec_log_addr(struct cec_adapter *adap, u8 logical_addr)
 {
 	struct dw_hdmi_qp_cec *cec = cec_get_drvdata(adap);
 
+	if (!cec->cec_enable)
+		return 0;
+
 	if (logical_addr == CEC_LOG_ADDR_INVALID)
 		cec->addresses = 0;
 	else
@@ -155,6 +159,9 @@ static int dw_hdmi_qp_cec_transmit(struct cec_adapter *adap, u8 attempts,
 	struct dw_hdmi_qp_cec *cec = cec_get_drvdata(adap);
 	unsigned int i;
 	u32 val;
+
+	if (!cec->cec_enable)
+		return 0;
 
 	for (i = 0; i < msg->len; i++) {
 		if (!(i % 4))
@@ -251,6 +258,8 @@ static int dw_hdmi_qp_cec_enable(struct cec_adapter *adap, bool enable)
 	struct dw_hdmi_qp_cec *cec = cec_get_drvdata(adap);
 
 	if (!enable) {
+		if (!cec->cec_enable)
+			return 0;
 		dw_hdmi_qp_write(cec, 0, CEC_INT_MASK_N);
 		dw_hdmi_qp_write(cec, ~0, CEC_INT_CLEAR);
 		if (cec->wake_irq > 0 && cec->wake_en && cec->standby_en) {
@@ -267,11 +276,16 @@ static int dw_hdmi_qp_cec_enable(struct cec_adapter *adap, bool enable)
 			dw_hdmi_qp_wakeup_write(cec, 0x8004800d, CEC_PAT3);
 			dw_hdmi_qp_wakeup_mod(cec, WAKEUP_EN, WAKEUP_EN, CEC_CTRL);
 		} else {
+			cec->addresses = 0;
+			dw_hdmi_qp_write(cec, cec->addresses, CEC_ADDR);
 			cec->ops->disable(cec->hdmi);
+			cec->cec_enable = false;
 		}
 	} else {
 		unsigned int irqs;
 
+		if (cec->cec_enable)
+			return 0;
 		if (cec->wake_irq > 0) {
 			cec->ops->set_wakeup(cec->hdmi, false);
 			dw_hdmi_qp_wakeup_write(cec, 0xffff0000, CEC_IE);
@@ -280,6 +294,7 @@ static int dw_hdmi_qp_cec_enable(struct cec_adapter *adap, bool enable)
 		}
 
 		cec->ops->enable(cec->hdmi);
+		cec->cec_enable = true;
 
 		dw_hdmi_qp_write(cec, ~0, CEC_INT_CLEAR);
 		dw_hdmi_qp_write(cec, 1, CEC_LOCK_CONTROL);

commit 84fa7d6d9a27148c6888a62d4475b34adcc1e522

drm/bridge: dw-hdmi-qp-cec: Fix system crash caused by cec irq during resume
When system resume cec may not be enabled yet, but a cec
interrupt is triggered. Accessing CEC registers at this
time could cause the system to crash.

The crash stack is as follows:

[41052.640187][    C0] Internal error: synchronous external abort: 0000000096000010 [#1] PREEMPT SMP
[41052.642089][    C0] Modules linked in: bcmdhd(O) r8168
[41052.642554][    C0] CPU: 0 PID: 0 Comm: swapper/0 Tainted: G   M       O       6.1.157 #80
[41052.643286][    C0] Hardware name: Rockchip RK3588 NVR DEMO1 LP4 V21 Android Board (DT)
[41052.643995][    C0] pstate: 204000c9 (nzCv daIF +PAN -UAO -TCO -DIT -SSBS BTYPE=--)
[41052.644671][    C0] pc : regmap_mmio_read32le+0x8/0x20
[41052.645137][    C0] lr : regmap_mmio_read+0x44/0x70
[41052.645574][    C0] sp : ffffffc008003e10
[41052.645931][    C0] x29: ffffffc008003e10 x28: ffffffc00a079e00 x27: ffffffc00a08b100
[41052.646625][    C0] x26: 0000000000000001 x25: ffffffc00a493000 x24: ffffffc00a04eb70
[41052.647327][    C0] x23: ffffffc009d6a732 x22: ffffff81f3ebc000 x21: 0000000000004000
[41052.648028][    C0] x20: ffffff81f3f00f00 x19: ffffffc008003ec4 x18: ffffffc008005058
[41052.648728][    C0] x17: ffffffc1f0cda000 x16: ffffffc008000000 x15: 0000000000000010
[41052.649419][    C0] x14: 0000000000000010 x13: 0000000000000016 x12: 000000000000000a
[41052.650119][    C0] x11: 0000000000000000 x10: ffffffc0096fdb38 x9 : 0000000000000000
[41052.650810][    C0] x8 : ffffffc00c284000 x7 : 0000000000000000 x6 : 0000000000000000
[41052.651503][    C0] x5 : fffffffdef8cd7b0 x4 : ffffff81fad26f98 x3 : 0000000008e6a8c4
[41052.652196][    C0] x2 : ffffffc008003ec4 x1 : 0000000000004000 x0 : ffffff81f3f00f00
[41052.652889][    C0] Call trace:
[41052.653171][    C0]  regmap_mmio_read32le+0x8/0x20
[41052.653599][    C0]  _regmap_bus_reg_read+0x28/0x34
[41052.654037][    C0]  _regmap_read+0x18c/0x240
[41052.654432][    C0]  regmap_read+0x54/0x78
[41052.654804][    C0]  hdmi_readl+0x2c/0x58
[41052.655166][    C0]  dw_hdmi_qp_cec_hardirq+0x28/0x128
[41052.655625][    C0]  __handle_irq_event_percpu+0xa4/0x254
[41052.656108][    C0]  handle_irq_event+0x48/0xb4
[41052.656512][    C0]  handle_fasteoi_irq+0x10c/0x208
[41052.656950][    C0]  generic_handle_domain_irq+0x40/0x64
[41052.657423][    C0]  gic_handle_irq+0x1b4/0x314
[41052.657828][    C0]  call_on_irq_stack+0x48/0x70
[41052.658244][    C0]  el1_interrupt+0x88/0x170
[41052.658641][    C0]  el1h_64_irq_handler+0x18/0x24
[41052.659069][    C0]  el1h_64_irq+0x78/0x7c
[41052.659439][    C0]  arch_local_irq_enable+0x8/0x1c
[41052.659877][    C0]  default_idle_call+0x38/0x150
[41052.660296][    C0]  do_idle+0xf4/0x2c4
[41052.660648][    C0]  cpu_startup_entry+0x34/0x38
[41052.661062][    C0]  kernel_init+0x0/0x1a4
[41052.661434][    C0]  start_kernel+0x0/0x430
[41052.661810][    C0]  start_kernel+0x39c/0x430
[41052.662203][    C0]  __primary_switched+0xc4/0xcc

Fixes: c1851a5cf9fd ("drm/bridge: dw-hdmi-qp-cec: Fix crash when cec is disabled")

drivers/gpu/drm/bridge/synopsys/dw-hdmi-qp-cec.c
@@ -100,6 +100,7 @@ struct dw_hdmi_qp_cec {
 	struct cec_notifier *notify;
 	int irq;
 	int wake_irq;
+	spinlock_t cec_lock;
 	struct mutex wake_lock;
 	struct input_dev *devinput;
 	struct miscdevice misc_dev;
@@ -139,9 +140,13 @@ static void dw_hdmi_qp_wakeup_mod(struct dw_hdmi_qp_cec *cec, u32 val, u32 mask,
 static int dw_hdmi_qp_cec_log_addr(struct cec_adapter *adap, u8 logical_addr)
 {
 	struct dw_hdmi_qp_cec *cec = cec_get_drvdata(adap);
+	unsigned long flags;
 
-	if (!cec->cec_enable)
+	spin_lock_irqsave(&cec->cec_lock, flags);
+	if (!cec->cec_enable) {
+		spin_unlock_irqrestore(&cec->cec_lock, flags);
 		return 0;
+	}
 
 	if (logical_addr == CEC_LOG_ADDR_INVALID)
 		cec->addresses = 0;
@@ -149,6 +154,7 @@ static int dw_hdmi_qp_cec_log_addr(struct cec_adapter *adap, u8 logical_addr)
 		cec->addresses |= BIT(logical_addr) | BIT(15);
 
 	dw_hdmi_qp_write(cec, cec->addresses, CEC_ADDR);
+	spin_unlock_irqrestore(&cec->cec_lock, flags);
 
 	return 0;
 }
@@ -159,9 +165,13 @@ static int dw_hdmi_qp_cec_transmit(struct cec_adapter *adap, u8 attempts,
 	struct dw_hdmi_qp_cec *cec = cec_get_drvdata(adap);
 	unsigned int i;
 	u32 val;
+	unsigned long flags;
 
-	if (!cec->cec_enable)
+	spin_lock_irqsave(&cec->cec_lock, flags);
+	if (!cec->cec_enable) {
+		spin_unlock_irqrestore(&cec->cec_lock, flags);
 		return 0;
+	}
 
 	for (i = 0; i < msg->len; i++) {
 		if (!(i % 4))
@@ -179,6 +189,7 @@ static int dw_hdmi_qp_cec_transmit(struct cec_adapter *adap, u8 attempts,
 
 	dw_hdmi_qp_write(cec, msg->len - 1, CEC_TX_CNT);
 	dw_hdmi_qp_write(cec, CEC_CTRL_START, CEC_TX_CONTROL);
+	spin_unlock_irqrestore(&cec->cec_lock, flags);
 
 	return 0;
 }
@@ -187,11 +198,21 @@ static irqreturn_t dw_hdmi_qp_cec_hardirq(int irq, void *data)
 {
 	struct cec_adapter *adap = data;
 	struct dw_hdmi_qp_cec *cec = cec_get_drvdata(adap);
-	u32 stat = dw_hdmi_qp_read(cec, CEC_INT_STATUS);
+	unsigned long flags;
+	u32 stat;
 	irqreturn_t ret = IRQ_HANDLED;
 
-	if (stat == 0)
-		return IRQ_NONE;
+	spin_lock_irqsave(&cec->cec_lock, flags);
+	if (!cec->cec_enable) {
+		spin_unlock_irqrestore(&cec->cec_lock, flags);
+		return IRQ_HANDLED;
+	}
+
+	stat = dw_hdmi_qp_read(cec, CEC_INT_STATUS);
+	if (stat == 0) {
+		spin_unlock_irqrestore(&cec->cec_lock, flags);
+		return IRQ_HANDLED;
+	}
 
 	dw_hdmi_qp_write(cec, stat, CEC_INT_CLEAR);
 
@@ -233,6 +254,7 @@ static irqreturn_t dw_hdmi_qp_cec_hardirq(int irq, void *data)
 
 		ret = IRQ_WAKE_THREAD;
 	}
+	spin_unlock_irqrestore(&cec->cec_lock, flags);
 
 	return ret;
 }
@@ -256,13 +278,17 @@ static irqreturn_t dw_hdmi_qp_cec_thread(int irq, void *data)
 static int dw_hdmi_qp_cec_enable(struct cec_adapter *adap, bool enable)
 {
 	struct dw_hdmi_qp_cec *cec = cec_get_drvdata(adap);
+	unsigned long flags;
 
 	if (!enable) {
 		if (!cec->cec_enable)
 			return 0;
+
+		spin_lock_irqsave(&cec->cec_lock, flags);
 		dw_hdmi_qp_write(cec, 0, CEC_INT_MASK_N);
 		dw_hdmi_qp_write(cec, ~0, CEC_INT_CLEAR);
 		if (cec->wake_irq > 0 && cec->wake_en && cec->standby_en) {
+			spin_unlock_irqrestore(&cec->cec_lock, flags);
 			cec->ops->set_wakeup(cec->hdmi, true);
 			dw_hdmi_qp_wakeup_mod(cec, __ffs(cec->addresses), DST_PAT, CEC_LADR_PAT);
 			dw_hdmi_qp_wakeup_write(cec, 0xffffffff, CEC_IS);
@@ -278,14 +304,17 @@ static int dw_hdmi_qp_cec_enable(struct cec_adapter *adap, bool enable)
 		} else {
 			cec->addresses = 0;
 			dw_hdmi_qp_write(cec, cec->addresses, CEC_ADDR);
-			cec->ops->disable(cec->hdmi);
 			cec->cec_enable = false;
+			spin_unlock_irqrestore(&cec->cec_lock, flags);
+
+			cec->ops->disable(cec->hdmi);
 		}
 	} else {
 		unsigned int irqs;
 
 		if (cec->cec_enable)
 			return 0;
+
 		if (cec->wake_irq > 0) {
 			cec->ops->set_wakeup(cec->hdmi, false);
 			dw_hdmi_qp_wakeup_write(cec, 0xffff0000, CEC_IE);
@@ -294,18 +323,21 @@ static int dw_hdmi_qp_cec_enable(struct cec_adapter *adap, bool enable)
 		}
 
 		cec->ops->enable(cec->hdmi);
+
+		spin_lock_irqsave(&cec->cec_lock, flags);
 		cec->cec_enable = true;
 
 		dw_hdmi_qp_write(cec, ~0, CEC_INT_CLEAR);
 		dw_hdmi_qp_write(cec, 1, CEC_LOCK_CONTROL);
 
-		dw_hdmi_qp_cec_log_addr(cec->adap, CEC_LOG_ADDR_INVALID);
-
 		irqs = CEC_STAT_LINE_ERR | CEC_STAT_NACK | CEC_STAT_EOM |
 		       CEC_STAT_DONE;
 		dw_hdmi_qp_write(cec, ~0, CEC_INT_CLEAR);
 		dw_hdmi_qp_write(cec, irqs, CEC_INT_MASK_N);
+		spin_unlock_irqrestore(&cec->cec_lock, flags);
+		dw_hdmi_qp_cec_log_addr(cec->adap, CEC_LOG_ADDR_INVALID);
 	}
+
 	return 0;
 }
 
@@ -488,6 +520,7 @@ static int dw_hdmi_qp_cec_probe(struct platform_device *pdev)
 	cec->irq = data->irq;
 	cec->wake_irq = data->wake_irq;
 	cec->cec_wakeup_mem = data->cec_wakeup_mem;
+	spin_lock_init(&cec->cec_lock);
 	mutex_init(&cec->wake_lock);
 
 	platform_set_drvdata(pdev, cec);
@@ -525,7 +558,7 @@ static int dw_hdmi_qp_cec_probe(struct platform_device *pdev)
 
 	ret = devm_request_threaded_irq(&pdev->dev, cec->irq,
 					dw_hdmi_qp_cec_hardirq,
-					dw_hdmi_qp_cec_thread, IRQF_SHARED,
+					dw_hdmi_qp_cec_thread, IRQF_ONESHOT,
 					"dw-hdmi-qp-cec", cec->adap);
 	if (ret < 0) {
 		dev_err(&pdev->dev, "cec request irq thread failed\n");