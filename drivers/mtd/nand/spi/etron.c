61a9ab11a8982f00ee56f917aa28e839bd61c84f
Jon Lin <jon.lin@rock-chips.com>
2026-05-21 18:44:46 +0800

    mtd: spinand: Etron: Support new device EM73F044VCE-OH
    Change-Id: Iec17359ef9a43f9925458a1ea6f92dd44d1e4068
    Signed-off-by: Jon Lin <jon.lin@rock-chips.com>

--- drivers/mtd/nand/spi/etron.c
+++ drivers/mtd/nand/spi/etron.c
@@ -237,6 +237,15 @@ static const struct spinand_info etron_spinand_table[] = {
 					      &update_cache_variants),
 		     SPINAND_HAS_QE_BIT,
 		     SPINAND_ECCINFO(&em73e044vck_ooblayout, em73c044vcf_oh_ecc_get_status)),
+	SPINAND_INFO("EM73F044VCE-OH",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_ADDR, 0x53),
+		     NAND_MEMORG(1, 4096, 256, 64, 4096, 40, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     SPINAND_HAS_QE_BIT,
+		     SPINAND_ECCINFO(&em73e044vck_ooblayout, em73c044vcf_oh_ecc_get_status)),
 };
 
 static const struct spinand_manufacturer_ops etron_spinand_manuf_ops = {
