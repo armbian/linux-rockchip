90eefde08d7db6c3906f6b490148b794fa6385bd
Chandler Chen <chandler.chen@rock-chips.com>
2026-05-29 16:51:14 +0800

    video: rockchip: mpp: fix vepu510 spurious wdg timeout after page fault
    Fix bug:
    There is a hw bug on VEPU510, if a task encounters a watchdog timeout
    caused by a page fault, the next task will immediately trigger another
    watchdog timeout right after the encoder is started, even though the
    encoding can complete normally.
    
    Solution:
    Mark next task wdg_en disabled when a wdg timeout interrupt is detected,
    so the next task will not be affected by the spurious watchdog timeout.
    
    Change-Id: Id6e1ee5b4c2afef8b33635645348122f28638e5b
    Signed-off-by: Chandler Chen <chandler.chen@rock-chips.com>

--- drivers/video/rockchip/mpp/mpp_rkvenc2.c
+++ drivers/video/rockchip/mpp/mpp_rkvenc2.c
@@ -342,6 +342,8 @@ struct rkvenc_dev {
 	struct rockchip_opp_info opp_info;
 	struct monitor_dev_info *mdev_info;
 #endif
+	/* for wdg timeout hw issue */
+	u32 next_wdg_off;
 };
 
 struct rkvenc_ccu {
@@ -1570,6 +1572,16 @@ static int rkvenc_run(struct mpp_dev *mpp, struct mpp_task *mpp_task)
 		} else {
 			mpp_write(mpp, 0x300, enc_pic | BIT(30));
 		}
+		/*
+		 * Fix bug:
+		 * after page fault happens,
+		 * wdg timeout will rise immediately when next task starts,
+		 * so we need to disable wdg_en bit to avoid wdg timeout happens to the next task.
+		 */
+		if (enc->next_wdg_off) {
+			mpp_write(mpp, 0x20, mpp_read(mpp, 0x20) & ~BIT(8));
+			enc->next_wdg_off = 0;
+		}
 	}
 
 	/* Flush the register before the start the device */
@@ -1798,6 +1810,7 @@ static int rkvenc_isr(struct mpp_dev *mpp)
 	struct rkvenc_task *task;
 	struct mpp_task *mpp_task;
 	struct rkvenc_dev *enc = to_rkvenc_dev(mpp);
+	struct rkvenc_hw_info *hw = enc->hw_info;
 	struct mpp_taskqueue *queue = mpp->queue;
 	unsigned long core_idle;
 
@@ -1827,6 +1840,11 @@ static int rkvenc_isr(struct mpp_dev *mpp)
 		/* dump register */
 		if (mpp_debug_unlikely(DEBUG_DUMP_ERR_REG))
 			mpp_task_dump_hw_reg(mpp);
+
+		/* mark next task wdg timeout disabled if wdg timeout irq is triggered */
+		if (hw->vepu_type == RKVENC_VEPU_510 &&
+		    task->irq_status & INT_STA_WDG_STA)
+			enc->next_wdg_off = 1;
 	}
 
 	mpp_task_finish(mpp_task->session, mpp_task);
