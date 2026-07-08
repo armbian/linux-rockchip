f9789ad1e5bd0445ea338de86a878517998d126d
Damon Ding <damon.ding@rock-chips.com>
2026-07-08 14:50:20 +0800

    drm/rockchip: vop2: Use rkiommu 1.0 instead of rkiommu 2.0 for RK3576
    Switch RK3576 default IOMMU to rkiommu 1.0 for two main reasons:
    1. Long-term engineering tests show rkiommu 1.0 provides better
       stability and performance than rkiommu 2.0 on RK3576, so use 1.0
       unconditionally by default.
    2. Under MOS Android + Linux, Android panic/reboot easily triggers
       IOMMU page faults. When rkiommu v2 is enabled, iommu0 may fail full
       reset randomly after Android display restarts.
    
    Fixes: 8379fd75553a ("drm/rockchip: vop2: add support rk3576")
    Change-Id: Iba5889147edc95bfd8f82feb861595a9ac1db1a3
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_vop2.c
@@ -5523,15 +5523,7 @@ static void vop2_initial(struct drm_crtc *crtc)
 			vop2_mask_write(vop2, 0x700, 0x3, 4, 0, 0, true);
 
 		if (vop2->version == VOP_VERSION_RK3576) {
-			/* reserved plane mode will enable iommu bypass for rtos reserved plane display,
-			 * but rkiommu 2.0 can't support iommu bypass function, so use rkiommu 1.0
-			 * at reserved plane mode by default, others will use rkiommu 2.0 by default.
-			 */
-			if (vop2->enable_reserved_plane)
-				VOP_CTRL_SET(vop2, rkmmu_v2_en, 0);
-			else /* Default use rkiommu 2.0 for axi0 */
-				VOP_CTRL_SET(vop2, rkmmu_v2_en, 1);
-
+			VOP_CTRL_SET(vop2, rkmmu_v2_en, 0);
 			if (vop2->merge_irq == true)
 				VOP_CTRL_SET(vop2, vp_intr_merge_en, 1);
 			VOP_CTRL_SET(vop2, lut_use_axi1, 0);
