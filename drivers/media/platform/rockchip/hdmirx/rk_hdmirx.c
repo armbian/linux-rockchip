437126a65d82a8ebf753bdad4e19d0743d739aea
SuperKali <hello@superkali.me>
2026-07-09 09:21:55 +0200

    media: rockchip: hdmirx: quiet signal-not-lock polling spam
    When a source is reported present but never produces a lockable signal,
    hdmirx_wait_lock_and_get_timing logs two v4l2_err lines on every poll,
    flooding the kernel log every few seconds. Demote them to v4l2_dbg like
    the pull-out path just above, keeping the log clean while the receiver
    keeps retrying.
    
    Signed-off-by: SuperKali <hello@superkali.me>

--- drivers/media/platform/rockchip/hdmirx/rk_hdmirx.c
+++ drivers/media/platform/rockchip/hdmirx/rk_hdmirx.c
@@ -1724,9 +1724,9 @@ static int hdmirx_wait_lock_and_get_timing(struct rk_hdmirx_dev *hdmirx_dev)
 	}
 
 	if (i == WAIT_SIGNAL_LOCK_TIME) {
-		v4l2_err(v4l2_dev, "%s signal not lock, tmds_clk_ratio:%d\n",
+		v4l2_dbg(1, debug, v4l2_dev, "%s signal not lock, tmds_clk_ratio:%d\n",
 				__func__, hdmirx_dev->tmds_clk_ratio);
-		v4l2_err(v4l2_dev, "%s mu_st:%#x, scdc_st:%#x, dma_st10:%#x\n",
+		v4l2_dbg(1, debug, v4l2_dev, "%s mu_st:%#x, scdc_st:%#x, dma_st10:%#x\n",
 				__func__, mu_status, scdc_status, dma_st10);
 		v4l2_ctrl_s_ctrl(hdmirx_dev->content_type, V4L2_DV_IT_CONTENT_TYPE_NO_ITC);
 
